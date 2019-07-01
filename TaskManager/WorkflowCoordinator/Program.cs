3b7f55476075f17f77874af538e50efe38350f5d
Rossall Sandford <rossall.sandford@ukho.gov.uk>
2019-07-01 15:40:39 +0100

    Initial set up of NSB Webjob project.
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- TaskManager/WorkflowCoordinator/Program.cs
+++ TaskManager/WorkflowCoordinator/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WorkflowCoordinator
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureWebJobs(b =>
+                {
+                    b.UseHostId("TBC")
+                        .AddAzureStorageCoreServices();
+                })
+                .ConfigureAppConfiguration(b =>
+                {
+                    // configure me
+                })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Debug);
+
+                })
+                .ConfigureServices(serviceCollection =>
+                {
+                    // TODO ... what scope?
+                    serviceCollection.AddScoped<IJobHost, TestHost>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
         }
     }
 }
