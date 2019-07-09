ef5be6a6fdfd5bd1f1397ad26ec14930eaa3528d
Greg Williams <Greg.Williams@UKHO.gov.uk>
2019-07-09 17:24:24 +0100

    Create SourceDocumentCoordinator & SourceDocumentCoordinatorTests

---
+++ TaskManager/SourceDocumentCoordinator/Config/SecretsConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace SourceDocumentCoordinator.Config
+{
+    public class SecretsConfig
+    { 
+        public string NsbDataSource { get; set; }
+
+        public string NsbInitialCatalog { get; set; }
+
+        // e.g.
+        //public IEnumerable<string> Validate()
+        //{
+        //     if ....
+        //     yield return $"Valid ... not ... {nameof(MySecret)}";
+        //}
+    }
+}
