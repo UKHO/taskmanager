dd503ccd91e3485498cc33e7e8a159db7607a759
Rossall Sandford <rossall.sandford@ukho.gov.uk>
2019-07-11 14:49:02 +0100

    Add healthcheck controller as we cannot use the nice healthcheck middleware in 2.2! Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com> Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ TaskManager/DataServices/Controllers/HealthcheckController.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataServices.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HealthcheckController : ControllerBase
+    {
+        [HttpGet]
+        [Route("healthcheck")]
+        public IActionResult Get()
+        {
+            return Ok(Assembly.GetExecutingAssembly().GetName().Name + " is ok.");
+        }
+
+        [HttpGet]
+        [Route("healthcheck/sdra")]
+        public IActionResult GetSdra()
+        {
+            return Ok("SDRA is ok.");
+        }
+    }
+}
