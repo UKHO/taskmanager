d56fda3fef47065d4238102e044610b88aca0452
Rossall Sandford <rossall.sandford@ukho.gov.uk>
2019-07-12 13:51:20 +0100

    Remove SDRA healthchecks from healthcheck controller. Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- TaskManager/DataServices/Controllers/HealthcheckController.cs
+++ TaskManager/DataServices/Controllers/HealthcheckController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SDRAAssessmentWebService;
 
 namespace DataServices.Controllers
 {
@@ -10,18 +8,10 @@ namespace DataServices.Controllers
     [ApiController]
     public class HealthcheckController : ControllerBase
     {
-        public readonly SDRAAssessmentWebService.SDRAExternalInterfaceAssessmentWebServiceSoap _sdraAssessmentWebService;
-
-        public readonly SDRADataAccessWebService.SDRAExternalInterfaceDataAccessWebServiceSoap _sdraDataAccessWebService;
-
         private readonly ILogger _logger;
 
-        public HealthcheckController(SDRAAssessmentWebService.SDRAExternalInterfaceAssessmentWebServiceSoap sdraAssessmentWebService,
-            SDRADataAccessWebService.SDRAExternalInterfaceDataAccessWebServiceSoap sdraDataAccessWebService,
-            ILogger<HealthcheckController> logger)
+        public HealthcheckController(ILogger<HealthcheckController> logger)
         {
-            _sdraAssessmentWebService = sdraAssessmentWebService;
-            _sdraDataAccessWebService = sdraDataAccessWebService;
             _logger = logger;
         }
 
@@ -31,45 +21,6 @@ namespace DataServices.Controllers
         {
             _logger.LogInformation("Healthcheck GET");
             return Ok(Assembly.GetExecutingAssembly().GetName().Name + " is ok.");
-        }
-
-        [HttpGet]
-        [Route("healthcheck/SDRAAssessment")]
-        public IActionResult GetSDRAAssessmentWebService()
-        {
-            _logger.LogInformation("Healthcheck/SDRAAssessment GET");
-            try
-            {
-                //TODO: Change the SDRA method to request to something less intensive
-                var result = _sdraAssessmentWebService.GetDocumentsForAssessmentAsync(
-                    new GetDocumentsForAssessmentRequest()).Result;
-
-                return Ok("SDRAAssessmentWebService is ok.");
-            }
-            catch (Exception e)
-            {
-                return base.StatusCode(500, $"SDRAAssessmentWebService is NOT ok. {e.ToString()}");
-            }
-            
-            
-        }
-
-        [HttpGet]
-        [Route("healthcheck/SDRAData")]
-        public IActionResult GetSDRADataAccessWebService()
-        {
-            _logger.LogInformation("Healthcheck/SDRAData GET");
-            try
-            {
-                //TODO: Change the SDRA method to request to something less intensive
-                var result = _sdraDataAccessWebService.GetAllDocumentTypesAsync().Result;
-
-                return Ok("GetSDRADataAccessWebService is ok.");
-            }
-            catch (Exception e)
-            {
-                return base.StatusCode(500, $"GetSDRADataAccessWebService is NOT ok. {e.ToString()}");
-            }
         }
     }
 }
