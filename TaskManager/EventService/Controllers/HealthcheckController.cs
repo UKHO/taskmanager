8b0755754b318dbc73ae49c36026e171b65772bc
Rossall Sandford <rossall.sandford@ukho.gov.uk>
2019-07-15 13:52:02 +0100

    Added healthcheck controller Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ TaskManager/EventService/Controllers/HealthcheckController.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventService.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HealthcheckController : ControllerBase
+    {
+        [HttpGet]
+        [Route("healthcheck")]
+        public IActionResult Get()
+        {
+            return Ok(Assembly.GetExecutingAssembly().GetName().Name + " is ok.");
+        }
+    }
+}
