81905d92d21ca7f35bc13710dc430c391a5e0b1c
Greg Williams <Greg.Williams@UKHO.gov.uk>
2019-07-17 14:39:44 +0100

    Add logging & healthcheck controller

--- TaskManager/Portal/Program.cs
+++ TaskManager/Portal/Program.cs
@@ -19,6 +19,13 @@ namespace Portal
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddAzureWebAppDiagnostics();
+                });
     }
 }
