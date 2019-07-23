f7fe070cf24f9a0ea84ba70b41454edaf461adc6
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-07-23 15:59:36 +0100

    Try to read from CSV to populate DataTable
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>

--- src/Portal/Portal/Pages/Index.cshtml.cs
+++ src/Portal/Portal/Pages/Index.cshtml.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
 
 namespace Portal.Pages
 {
@@ -11,7 +9,33 @@ namespace Portal.Pages
     {
         public void OnGet()
         {
+            var rows = System.IO.File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "ExampleDataCSV.csv"));
 
+            var lst = new List<Models.LandingPage>();
+
+            foreach (var row in rows.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    lst.Add(new Models.LandingPage
+                    {
+                        ProcessId = row.Split(',')[0],
+                        DaysToDmEndDate = Convert.ToInt32(row.Split(',')[1]),
+                        DmEndDate = Convert.ToDateTime(row.Split(',')[2]),
+                        DaysOnHold = Convert.ToInt32(row.Split(',')[3]),
+                        RsdraNo = row.Split(',')[4],
+                        SourceName = row.Split(',')[5],
+                        Workspace = row.Split(',')[6],
+                        TaskType = row.Split(',')[7],
+                        TaskStage = row.Split(',')[8],
+                        Assessor = row.Split(',')[9],
+                        Verifier = row.Split(',')[10],
+                        Team = row.Split(',')[11]
+                    });
+                }
+            }
+
+            var no = lst.Count;
         }
     }
 }
