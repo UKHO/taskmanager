48eb7a2d8db8ae70caa2a25b3f896f94877f74f1
Tom Guppy <tom.guppy@ukho.gov.uk>
2019-07-31 16:35:45 +0100

    Create example test using rest-assured
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
    Co-authored-by: Tom Guppy <50492647+TomGuppy@users.noreply.github.com>
    Co-authored-by: Greg Zealley <47225721+gregzealley@users.noreply.github.com>

--- src/DataServices/DataServices.ApiTests/src/test/java/dataservice/Tests.java
+++ src/DataServices/DataServices.ApiTests/src/test/java/dataservice/Tests.java
@@ -1,4 +1,17 @@
 package dataservice;
 
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class Tests {
+
+    @Test
+    public void ExampleTest() {
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then()
+                .statusCode(200);
+    }
 }
