aaa6de02019ad959808daace0f6b3ffdf4f01c85
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-08-02 15:24:57 +0100

    Initial database table structure for what we know so far.
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ src/Databases/Database.SQL/Tables/AssessmentData.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[AssessmentData]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[SdocId] INT NOT NULL,
+	[RsdraNumber] NVARCHAR(50) NOT NULL,
+	[SourceDocumentName] NVARCHAR(255) NOT NULL,
+	[ReceiptDate] DATETIME NOT NULL,
+	[ToSdoDate] DATETIME NULL,
+	[EffectiveStartDate] DATETIME NULL,
+	[TeamDistributedTo] NVARCHAR(10) NULL,
+	[SourceDocumentType] NVARCHAR(255) NULL,
+	[SourceNature] NVARCHAR(20) NULL,
+	[Datum] NVARCHAR(20) NULL
+)
+
