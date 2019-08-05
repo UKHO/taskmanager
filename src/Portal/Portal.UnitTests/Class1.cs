e88a13ff6c2b6fe9f2f7a48d47a093dde1a7a92b
Greg Williams <Greg.Williams@UKHO.gov.uk>
2019-08-05 11:54:49 +0100

    Add dummy unit test to Portal.UnitTests
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>

---
+++ src/Portal/Portal.UnitTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Portal.UnitTests
+{
+
+    [TestFixture]
+    class Class1
+    {
+        [Test]
+        public void Testing()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
