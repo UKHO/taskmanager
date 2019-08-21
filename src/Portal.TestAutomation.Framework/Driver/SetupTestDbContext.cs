d2b199f9dffe6050642d2102d3c9abe6442db8bb
Ben Hall <3179852+benbhall@users.noreply.github.com>
2019-08-21 15:31:20 +0100

    Inject dbcontext for test data into specflow steps
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>

---
+++ src/Portal.TestAutomation.Framework/Driver/SetupTestDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BoDi;
+using Microsoft.EntityFrameworkCore;
+using TechTalk.SpecFlow;
+using WorkflowDatabase.EF;
+
+namespace Portal.TestAutomation.Framework.Driver
+{
+    [Binding]
+    internal sealed class SetupTestDbContext
+    {
+        private readonly IObjectContainer _objectContainer;
+
+        public SetupTestDbContext(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeScenario(Order = 1)]
+        public void InitializeDbContext()
+        {
+
+            var dbContextOptions = new DbContextOptionsBuilder<WorkflowDbContext>()
+                .UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=taskmanager-dev-workflowdatabase;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False")
+                .Options;
+
+            var dbContext = new WorkflowDbContext(dbContextOptions);
+
+            _objectContainer.RegisterInstanceAs<WorkflowDbContext>(dbContext);
+        }
+    }
+}
