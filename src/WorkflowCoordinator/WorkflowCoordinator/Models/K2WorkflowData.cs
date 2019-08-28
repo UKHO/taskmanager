91842d1388644ebe1ee0fd3436f960c96161b61e
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-08-28 18:19:14 +0100

    Added K2WorkflowData to receive workflows data when getting all workflows

---
+++ src/WorkflowCoordinator/WorkflowCoordinator/Models/K2WorkflowData.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace WorkflowCoordinator.Models
+{
+    internal class K2WorkflowData
+    {
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [JsonProperty("defaultVersionId")]
+        public int DefaultVersionId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("folder")]
+        public string Folder { get; set; }
+
+        [JsonProperty("systemName")]
+        public string SystemName { get; set; }
+    }
+}
