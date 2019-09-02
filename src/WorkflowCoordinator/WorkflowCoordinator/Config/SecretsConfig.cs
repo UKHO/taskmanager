671edcc1a0c2d63f90728bf92a57ad2f27e91de4
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-09-02 14:40:17 +0100

    Added K2 to KeyVault
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- src/WorkflowCoordinator/WorkflowCoordinator/Config/SecretsConfig.cs
+++ src/WorkflowCoordinator/WorkflowCoordinator/Config/SecretsConfig.cs
@@ -1,10 +1,17 @@
-﻿namespace WorkflowCoordinator.Config
+﻿using NServiceBus;
+
+namespace WorkflowCoordinator.Config
 {
     public class SecretsConfig
     {
         public string NsbDataSource { get; set; }
 
         public string NsbInitialCatalog { get; set; }
+
+        public string NsbToK2ApiUsername { get; set; }
+
+
+        public string NsbToK2ApiPassword { get; set; }
 
     }
 }
