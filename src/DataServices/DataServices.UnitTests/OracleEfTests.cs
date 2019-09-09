1088961723bad0e8e62eff2abf439a82de1692cb
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-09-09 12:41:53 +0100

    Add unit test to ensure the controller method returns assessment data via EF core
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>

--- src/DataServices/DataServices.UnitTests/OracleEfTests.cs
+++ src/DataServices/DataServices.UnitTests/OracleEfTests.cs
@@ -1,19 +1,60 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DataServices.Controllers;
+using DataServices.Models;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace DataServices.UnitTests
 {
     public class OracleEfTests
     {
+        private SdraDbContext _dbContext;
+        private ILogger<DataAccessApiController> _fakeLogger;
+
         [SetUp]
         public void Setup()
         {
+            var dbContextOptions = new DbContextOptionsBuilder<SdraDbContext>()
+                .UseInMemoryDatabase(databaseName: "inmemory")
+                .Options;
+
+            _dbContext = new SdraDbContext(dbContextOptions);
+
+            _fakeLogger = A.Fake<ILogger<DataAccessApiController>>();
         }
 
         [Test]
-        public void Test1()
+        public void Test_GetDocumentAssessmentData_returns_assessment_data_for_given_sdoc_Id()
         {
+            var controller = new DataAccessApiController(_dbContext, _fakeLogger);
 
-            Assert.Pass();
+            _dbContext.AssessmentData.AddAsync(new DocumentAssessmentData()
+            {
+               SdocId = 1871160,
+               Datum = null,
+               DocumentNature = "Textual",
+               DocumentType = "Correspondence (Letter / Email / Fax / Signal) - requiring action",
+               EffectiveStartDate = new DateTime(2017, 03, 27),
+               Name = "COR_IN_GBENC_GB303790_000_P007_07-03-17",
+               Notes = null,
+               ReceiptDate = new DateTime(17, 03, 30),
+               SDODate = new DateTime(17,03,30),
+               SourceName = "RSDRA2017000073439",
+               Team = "HDB Master Data"
+            });
+            _dbContext.SaveChanges();
+
+            var actionResult = controller.GetDocumentAssessmentData("HDB", 1871160);
+            var objectResult = actionResult as ObjectResult;
+            var data = objectResult.Value as DocumentAssessmentData;
+
+            Assert.AreEqual(data.SdocId, 1871160);
         }
     }
 }
--- src/DataServices/DataServices/Models/SdraDbContext.cs
+++ src/DataServices/DataServices/Models/SdraDbContext.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace DataServices.Models
 {
