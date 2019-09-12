b08821899bc2fd4f77e5bfd4e2b77578f5f6c32d
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-09-12 08:45:05 +0100

    Initial scaffolding of SourceDocRetrieval saga

---
+++ src/SourceDocumentCoordinator/SourceDocumentCoordinator/Sagas/SourceDocumentRetrievalSaga.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Helpers;
+using Common.Messages.Commands;
+using NServiceBus;
+using NServiceBus.Logging;
+using WorkflowDatabase.EF;
+
+namespace SourceDocumentCoordinator.Sagas
+{
+    public class SourceDocumentRetrievalSaga : Saga<SourceDocumentRetrievalSagaData>, IAmStartedByMessages<InitiateSourceDocumentRetrievalCommand>
+    {
+        private readonly WorkflowDbContext _dbContext;
+        ILog log = LogManager.GetLogger<SourceDocumentRetrievalSaga>();
+
+        public SourceDocumentRetrievalSaga(WorkflowDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SourceDocumentRetrievalSagaData> mapper)
+        {
+            mapper.ConfigureMapping<InitiateSourceDocumentRetrievalCommand>(message => message.SourceDocumentId)
+                .ToSaga(sagaData => sagaData.SourceDocumentId);
+        }
+
+        public Task Handle(InitiateSourceDocumentRetrievalCommand message, IMessageHandlerContext context)
+        {
+            log.Debug($"Handling {nameof(InitiateSourceDocumentRetrievalCommand)}: {message.ToJSONSerializedString()}");
+
+            // Call GetDocumentForViewing method on DataServices API
+
+            // Send command to check GetDocumentRequestQueueStatus on DataServices API
+
+            // Once document has been fetched, call ClearDocumentRequestJobFromQueue on DataServices API and close saga...
+            
+            return null;
+        }
+    }
+}
---
+++ src/SourceDocumentCoordinator/SourceDocumentCoordinator/Sagas/SourceDocumentRetrievalSagaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Messages;
+using NServiceBus;
+
+namespace SourceDocumentCoordinator.Sagas
+{
+    public class SourceDocumentRetrievalSagaData : ContainSagaData, ICorrelate
+    {
+        public Guid CorrelationId { get; set; }
+        public int SourceDocumentId { get; set; }
+    }
+}
