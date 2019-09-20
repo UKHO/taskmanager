bdfabfd86e3a5ea6939f9cb051ba73d07a6fe4c9
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2019-09-20 13:43:13 +0100

    Create DB Assessment Review page skeleton
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>

---
+++ src/Portal/Portal/Pages/DbAssessment/Review.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portal.Pages.DbAssessment
+{
+    public class ReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/Portal/Portal/Pages/DbAssessment/_Comments.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portal.Pages.DbAssessment
+{
+    public class _CommentsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
