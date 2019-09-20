6a7a0013394cf5bfb10da11424f2f209255b126b
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2019-09-20 15:31:41 +0100

    Show dummy Task Information; remove Page directive from partial razor pages
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>

--- src/Portal/Portal/Pages/DbAssessment/Review.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/Review.cshtml.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Portal.Pages.DbAssessment
 {
     public class ReviewModel : PageModel
     {
+        public _TaskInformationModel TaskInformationModel { get; set; }
         public void OnGet()
         {
+            TaskInformationModel = new _TaskInformationModel();
+            TaskInformationModel.ProcessId = 98;
+            TaskInformationModel.DmEndDate = DateTime.Now;
+            TaskInformationModel.DmReceiptDate = DateTime.Now;
+            TaskInformationModel.EffectiveReceiptDate = DateTime.Now;
+            TaskInformationModel.ExternalEndDate = DateTime.Now;
+            TaskInformationModel.OnHold = 4;
+            TaskInformationModel.Ion = "2929";
+            TaskInformationModel.ActivityCode = "1272";
+            TaskInformationModel.SourceCategory = new SourceCategory() { SourceCategoryId = 1, Name = "zzzzz" };
 
+            TaskInformationModel.SourceCategories = new SelectList(new List<SourceCategory>()
+                {
+                    new SourceCategory() {SourceCategoryId = 0, Name = "asdas"},
+                    new SourceCategory() {SourceCategoryId = 1, Name = "zzzzz"}
+                }, "SourceCategoryId", "Name");
         }
     }
 }
--- src/Portal/Portal/Pages/DbAssessment/_TaskInformation.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/_TaskInformation.cshtml.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Portal.Pages.DbAssessment
 {
     public class _TaskInformationModel : PageModel
     {
+        public int ProcessId { get; set; }
+        public DateTime DmEndDate { get; set; }
+        public DateTime DmReceiptDate { get; set; }
+        public DateTime EffectiveReceiptDate { get; set; }
+        public DateTime ExternalEndDate { get; set; }
+        public int OnHold { get; set; }
+        public string Ion { get; set; }
+        public string ActivityCode { get; set; }
+        public SourceCategory SourceCategory { get; set; }
+
+        public SelectList SourceCategories { get; set; }
+
         public void OnGet()
         {
-
+            
         }
     }
 }
