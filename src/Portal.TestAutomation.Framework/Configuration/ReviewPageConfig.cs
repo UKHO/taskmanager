86d7058e5e7137f4a55fd2f4cff6dee0f3cea76d
Bonnie Poole <bonnie.poole@ukho.gov.uk>
2019-09-24 08:57:49 +0100

    BP Created POM for review page including feature, steps, and .cs file.  New class for review URLs and additions to azure for the review page url

---
+++ src/Portal.TestAutomation.Framework/Configuration/ReviewPageConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Portal.TestAutomation.Framework.Configuration
+{
+    public class ReviewPageConfig
+    {
+        public Uri ReviewPageUrl { get; set; }
+        public Uri LocalDevReviewPageUrl { get; set; }
+    }
+}
---
+++ src/Portal.TestAutomation.Specs/ReviewPageTasks.feature
@@ -0,0 +1,6 @@
+﻿Feature: ReviewPageTasks
+
+@mytag
+Scenario: The review page loads
+	Given I navigate to the review page 
+	 Then The review page has loaded
