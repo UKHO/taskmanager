6ef670b786cfc9e070109adb4646a865aedf97f5
Ben Hall <3179852+benbhall@users.noreply.github.com>
2019-09-24 10:03:32 +0100

    Fix Dataservices API in Azure
    - Upgrade Microsoft.Extensions.Logging.AzureAppServices 2.1.1 to 2.2.5 (missed in the Core 2.2 upgrade).
    
    Additional changes:
    
    - Added <EnableMSDeployAppOffline> to prevent deployment errors. Particularly noticable when publishing from VS during development. We need to look at deployment slots moving forward: https://docs.microsoft.com/en-gb/azure/app-service/deploy-staging-slots#custom-warm-up-before-swap
    - Added .SetCompatibilityVersion(CompatibilityVersion.Version_2_2) in Startup else it defaults to 2.0 and given we are developing a new appication in 2.2 we do not need to avoid breaking changes in 2.1/2.2.
    - Added ConfigHelpers.IsAzureDevelopment and via this enabled developer exception pages in Azure Dev
    - DI ILogger into Startup

--- src/DataServices/DataServices/Startup.cs
+++ src/DataServices/DataServices/Startup.cs
@@ -9,12 +9,14 @@
  */
 
 using System.IO;
+using Common.Helpers;
 using DataServices.Adapters;
 using DataServices.Config;
 using DataServices.Filters;
 using DataServices.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,7 @@ namespace DataServices
     public class Startup
     {
         private readonly IHostingEnvironment _hostingEnv;
+        private readonly ILogger _logger;
 
         private IConfiguration Configuration { get; }
 
@@ -40,9 +43,10 @@ namespace DataServices
         /// </summary>
         /// <param name="env"></param>
         /// <param name="configuration"></param>
-        public Startup(IHostingEnvironment env, IConfiguration configuration)
+        public Startup(IHostingEnvironment env, IConfiguration configuration, ILogger logger)
         {
             _hostingEnv = env;
+            _logger = logger;
             Configuration = configuration;
         }
 
@@ -52,9 +56,9 @@ namespace DataServices
         /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services
                 .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddJsonOptions(opts =>
                 {
                     opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
@@ -125,7 +129,7 @@ namespace DataServices
                     c.SwaggerEndpoint("/swagger-original.json", "SDRA API Original");
                 });
 
-            if (env.IsDevelopment())
+            if (ConfigHelpers.IsLocalDevelopment || ConfigHelpers.IsAzureDevelopment)
             {
                 app.UseDeveloperExceptionPage();
             }
