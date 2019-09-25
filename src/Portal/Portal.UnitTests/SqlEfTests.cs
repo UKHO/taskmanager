991176e010633533f14c19e33dd25d9d8a480203
Bonnie Poole <bonnie.poole@ukho.gov.uk>
2019-09-25 14:56:22 +0100

    Created unit test in portal to check comments are added to database

---
+++ src/Portal/Portal.UnitTests/SqlEfTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using WorkflowDatabase.EF;
+using WorkflowDatabase.EF.Models;
+
+namespace Portal.UnitTests
+{
+    [TestFixture]
+    public class SqlEfTests
+    {
+        private WorkflowDbContext _dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<WorkflowDbContext>()
+                .UseInMemoryDatabase(databaseName: "inmemory")
+                .Options;
+
+            _dbContext = new WorkflowDbContext(dbContextOptions);
+        }
+
+        [Test]
+        public void Test_Comment_can_be_added_to_db_using_ef()
+        {
+            _dbContext.Comment.AddAsync(new WorkflowDatabase.EF.Models.Comments()
+            {
+               CommentId = 1,
+               Created = new DateTime(),
+               ProcessId = 9876,
+               Text = "This is a comment",
+               Username = "Me",
+               WorkflowInstanceId = 555
+            });
+
+            _dbContext.SaveChanges();
+
+           
+            Assert.AreEqual(_dbContext.Comment.FirstAsync(c => c.CommentId == 1).Result.Text, "This is a comment");
+        }
+
+    }
+}
