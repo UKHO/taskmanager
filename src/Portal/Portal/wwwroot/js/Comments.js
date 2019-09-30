83cf12d6e72f79a9d67e4328c69e8f8bface8287
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-09-30 12:24:10 +0100

    Remove Render scripts and add comment validation
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>

--- src/Portal/Portal/wwwroot/js/Comments.js
+++ src/Portal/Portal/wwwroot/js/Comments.js
@@ -2,27 +2,35 @@
 
     $("#btnPostComment").click(function () {
 
-        var jsonData = {
-            "comment": $('#txtComment').val(), "processId": $('#hdnProcessId').val()
+        if ($('#txtComment').val() === "") {
+            $("#AddCommentError")
+                .html("<div class=\"alert alert-danger\" role=\"alert\">Please enter a comment.</div>");
+            $('#txtComment').focus();
+        } else {
+
+            var jsonData = {
+                "comment": $("#txtComment").val(),
+                "processId": $("#hdnProcessId").val()
             };
 
-        $.ajax({
-            type: 'GET',
-            url: "Review/?handler=CommentsPartial",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
-            },
-            contentType: 'application/json; charset=utf-8"',
-            data: jsonData,
-            success: function (result) {
-                $("#existingComments").html(result);
-                $('#addCommentModal').modal('hide');
-                $('.modal-backdrop').remove();
-                $('body').removeClass('modal-open');
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
+            $.ajax({
+                type: "GET",
+                url: "Review/?handler=CommentsPartial",
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+                },
+                contentType: 'application/json; charset=utf-8"',
+                data: jsonData,
+                success: function(result) {
+                    $("#existingComments").html(result);
+                    $("#addCommentModal").modal("hide");
+                    $(".modal-backdrop").remove();
+                    $("body").removeClass("modal-open");
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        }
     });
 });
