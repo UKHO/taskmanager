7f03ca468c65568bde90cf9fe747f9c41a789536
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-10-04 11:31:28 +0100

    Added delete assigned task functionality
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>
    Co-authored-by: Gareth Evans <51328957+gareth72evans@users.noreply.github.com>
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>

--- src/Portal/Portal/wwwroot/js/AssignTask.js
+++ src/Portal/Portal/wwwroot/js/AssignTask.js
@@ -2,9 +2,7 @@
 
     $(".deleteAssignTask").click(function () {
         var ordinal = $(this).data("ordinal");
-        var assignTaskId = $(this).data("assignTaskId");
-
-        alert(ordinal);
-        alert(assignTaskId);
+        var assignTaskId = $(this).data("assigntaskid"); // TODO: use when calling via ajax to update DB
+        $("#assignTask" + ordinal).remove();
     });
 });
