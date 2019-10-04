62eb0010da648605a00df2e9cc37616ae514b492
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-10-04 10:32:09 +0100

    WIP work for assign tasks

---
+++ src/Portal/Portal/wwwroot/js/AssignTask.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    $(".deleteAssignTask").click(function () {
+        var ordinal = $(this).data("ordinal");
+        var assignTaskId = $(this).data("assignTaskId");
+
+        alert(ordinal);
+        alert(assignTaskId);
+    });
+});
