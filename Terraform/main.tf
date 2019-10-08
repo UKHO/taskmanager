a31903fc592e294868bae86c76d1deb711c320d0
ShaunBresnahan <52528924+ShaunBresnahan@users.noreply.github.com>
2019-10-08 15:42:08 +0100

    Updating main.tf with backend information
    providing the  backend code for the main.tf. This will create the terraform state file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name           = "TaskmanagerDev-RG"
+    storage_account_name          = "taskmanagerdev"
+    container_name                = "tfstate"
+    key                           = "projectdev.tfstate"
+  }
+}
+
+locals {
+  nsb-role                         ="nsb"
+  api-role                         ="api"
+}
 
