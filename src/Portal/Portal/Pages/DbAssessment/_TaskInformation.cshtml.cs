da2282ce4ca42a166050229eabeadf0a875f6ce7
Ben Hall <3179852+benbhall@users.noreply.github.com>
2019-10-16 16:05:11 +0100

    WIP:
    - Upgrade BS3->BS4 using cards over panels but following flex otherwise
    - Moved most padding inline styles and use of <br> tags to new convenience classes from BS4
    - Switched to bootstrap's own class closing the for assigned tasks
    - Fixed seeded data that doesn't sit with new UNIQUE on db
    - Navigation buttons the same size
    - Footer text to same as UKHO design style
    - Fixed display name error for DmReceiptDate
    - Pulled in UKHO font JohnstonITC and plugged in as default. Font being pulle din via libman.json from UKHO styles package @ unpkg
    - (Hack) Hid navbar icon as we are not using (not sure why it was there)
    - Adopted most of the styling UKHO design system details for buttons
    - Made button sizing on nav bar consistent
    
    TODO:
    
    - Strange bolding on source docs table

--- src/Portal/Portal/Pages/DbAssessment/_TaskInformation.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/_TaskInformation.cshtml.cs
@@ -10,24 +10,32 @@ namespace Portal.Pages.DbAssessment
     {
         [DisplayName("Process Id:")]
         public int ProcessId { get; set; }
+
         [DisplayName("DM End Date:")]
         [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime DmEndDate { get; set; }
-        [DisplayName("Effective Receipt Date:")]
+
+        [DisplayName("DM Receipt Date:")]
         [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime DmReceiptDate { get; set; }
+
         [DisplayName("Effective Receipt Date:")]
         [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime EffectiveReceiptDate { get; set; }
+
         [DisplayName("External End Date:")]
         [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime ExternalEndDate { get; set; }
+
         [DisplayName("On Hold:")]
         public int OnHold { get; set; }
+
         [DisplayName("ION:")]
         public string Ion { get; set; }
+
         [DisplayName("Activity Code:")]
         public string ActivityCode { get; set; }
+
         [DisplayName("Source Category:")]
         public SourceCategory SourceCategory { get; set; }
 
@@ -35,7 +43,7 @@ namespace Portal.Pages.DbAssessment
 
         public void OnGet()
         {
-            
+
         }
     }
 }
