781773e522c96a2122cde4e76de009805c8b59ca
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-10-18 16:06:43 +0100

    Added url for PutAssessmentCompleted in azure config and updated Portal UriConfig
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ src/Portal/Portal/Configuration/UriConfig.cs
@@ -0,0 +1,21 @@
+﻿using Common.Helpers;
+
+using System;
+
+namespace Portal.Configuration
+{
+    public class UriConfig
+    {
+        public Uri DataAccessLocalhostBaseUri { get; set; }
+        public Uri DataServicesWebServiceBaseUri { get; set; }
+        public string DataServicesWebServiceAssessmentCompletedUri { get; set; }
+
+
+        public Uri BuildDataServicesUri(string callerCode, int sdocId, string comment)
+        {
+            return new Uri(
+                ConfigHelpers.IsLocalDevelopment ? DataAccessLocalhostBaseUri : DataServicesWebServiceBaseUri,
+                $@"{DataServicesWebServiceAssessmentCompletedUri}{callerCode}/{sdocId}/{comment}");
+        }
+    }
+}
