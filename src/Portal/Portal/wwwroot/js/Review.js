8c0cabb4be397dd9b51898c89c8357efaa25b0d1
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-10-22 16:33:47 +0100

    Do validation for the Terminate popup via submit handler
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- src/Portal/Portal/wwwroot/js/Review.js
+++ src/Portal/Portal/wwwroot/js/Review.js
@@ -13,6 +13,15 @@
     $('#btnTerminate').on('click', function () {
         $('#ConfirmTerminate').modal('show');
     });
+    
+    $("#terminatingReview").submit(function (event) {
+        if ($('#txtTerminateComment').val() === "") {
+            $("#ConfirmTerminateError")
+                .html("<div class=\"alert alert-danger\" role=\"alert\">Please enter a comment.</div>");
+            $('#txtTerminateComment').focus();
+        event.preventDefault();
+        }
+    });
 
     function getAssignTasks() {
         $.ajax({
