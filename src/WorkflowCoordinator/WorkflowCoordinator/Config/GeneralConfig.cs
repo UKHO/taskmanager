a44e6385faeeca3011becd5c52a10578f3c53888
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2019-11-06 12:19:24 +0000

    Add StartWorkflowInstanceEventHandler
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>

--- src/WorkflowCoordinator/WorkflowCoordinator/Config/GeneralConfig.cs
+++ src/WorkflowCoordinator/WorkflowCoordinator/Config/GeneralConfig.cs
@@ -6,6 +6,7 @@ namespace WorkflowCoordinator.Config
     {
         public string SourceDocumentCoordinatorName { get; set; }
         public string WorkflowCoordinatorName { get; set; }
+        public string EventServiceName { get; set; }
         public string CallerCode { get; set; }
         public int WorkflowCoordinatorAssessmentPollingIntervalSeconds { get; set; }
         public string LocalDbServer { get; set; }
---
+++ src/WorkflowCoordinator/WorkflowCoordinator/Handlers/StartWorkflowInstanceEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Messages.Events;
+using NServiceBus;
+
+namespace WorkflowCoordinator.Handlers
+{
+    public class StartWorkflowInstanceEventHandler : IHandleMessages<StartWorkflowInstanceEvent>
+    {
+        public Task Handle(StartWorkflowInstanceEvent message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
