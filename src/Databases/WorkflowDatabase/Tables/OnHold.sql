ffda7220d0cc61e5386c38ee2bb9015c40dce7d4
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2019-11-08 11:08:21 +0000

    Change OnHold datetime types to DATE
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>

--- src/Databases/WorkflowDatabase/Tables/OnHold.sql
+++ src/Databases/WorkflowDatabase/Tables/OnHold.sql
@@ -3,8 +3,8 @@
 	[OnHoldId] INT NOT NULL PRIMARY KEY IDENTITY, 
 	[WorkflowInstanceId] INT NOT NULL, 
     [ProcessId] INT NOT NULL, 
-    [OnHoldTime] DATETIME2 NOT NULL, 
-	[OffHoldTime] DATETIME2 NULL, 
+    [OnHoldTime] Date NOT NULL, 
+	[OffHoldTime] Date, 
 	[OnHoldUser] NVARCHAR(255) NOT NULL, 
     [OffHoldUser] NVARCHAR(255) NULL, 
     CONSTRAINT [FK_OnHold_WorkflowInstance] FOREIGN KEY ([WorkflowInstanceId]) REFERENCES [WorkflowInstance]([WorkflowInstanceId])
