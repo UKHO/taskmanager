a44df039942e5599ed5d2c2771d7f5301219bdb0
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2019-11-08 14:46:18 +0000

    Make OffHoldTime nullable
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>

--- src/Databases/WorkflowDatabase.EF/Models/OnHold.cs
+++ src/Databases/WorkflowDatabase.EF/Models/OnHold.cs
@@ -10,7 +10,7 @@ namespace WorkflowDatabase.EF.Models
         public int WorkflowInstanceId { get; set; }
         public int ProcessId { get; set; }
         public DateTime OnHoldTime { get; set; }
-        public DateTime OffHoldTime { get; set; }
+        public DateTime? OffHoldTime { get; set; }
         public string OnHoldUser{ get; set; }
         public string OffHoldUser { get; set; }
     }
