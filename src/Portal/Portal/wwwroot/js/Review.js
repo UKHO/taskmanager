3513766420f6ae1089ec1fc54b3902d6eba7dd35
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-11-14 10:06:31 +0000

    Moved getComments() function outside $(document).ready() so that it can be accessed from other js files

--- src/Portal/Portal/wwwroot/js/Review.js
+++ src/Portal/Portal/wwwroot/js/Review.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-    var processId = { "processId": $("#hdnProcessId").val() };
 
     getComments();
 
@@ -15,24 +14,25 @@
             event.preventDefault();
         }
     });
-
-    function getComments() {
-
-        $.ajax({
-            type: "GET",
-            url: "Review/?handler=RetrieveComments",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
-            },
-            contentType: "application/json; charset=utf-8",
-            data: processId,
-            success: function (result) {
-                $("#existingComments").html(result);
-            },
-            error: function (error) {
-                $("#AddCommentError")
-                    .html("<div class=\"alert alert-danger\" role=\"alert\">Failed to load comments.</div>");
-            }
-        });
-    }
 });
+
+function getComments() {
+    var processId = { "processId": $("#hdnProcessId").val() };
+
+    $.ajax({
+        type: "GET",
+        url: "Review/?handler=RetrieveComments",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        contentType: "application/json; charset=utf-8",
+        data: processId,
+        success: function (result) {
+            $("#existingComments").html(result);
+        },
+        error: function (error) {
+            $("#AddCommentError")
+                .html("<div class=\"alert alert-danger\" role=\"alert\">Failed to load comments.</div>");
+        }
+    });
+}
