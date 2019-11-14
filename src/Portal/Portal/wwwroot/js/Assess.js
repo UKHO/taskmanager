3e830286e2d89f74d8734b2b8b8fef54cace23e3
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2019-11-14 15:32:31 +0000

    Move getComments to global scope to account for it being called by TaskInformation
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>

--- src/Portal/Portal/wwwroot/js/Assess.js
+++ src/Portal/Portal/wwwroot/js/Assess.js
@@ -1,25 +1,24 @@
 ﻿$(document).ready(function () {
+    getComments();
+});
+
+function getComments() {
     var processId = { "processId": $("#hdnProcessId").val() };
 
-    getComments();
-
-    function getComments() {
-
-        $.ajax({
-            type: "GET",
-            url: "Review/?handler=RetrieveComments",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
-            },
-            contentType: "application/json; charset=utf-8",
-            data: processId,
-            success: function (result) {
-                $("#existingComments").html(result);
-            },
-            error: function (error) {
-                $("#AddCommentError")
-                    .html("<div class=\"alert alert-danger\" role=\"alert\">Failed to load comments.</div>");
-            }
-        });
-    }
-});
+    $.ajax({
+        type: "GET",
+        url: "Review/?handler=RetrieveComments",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        contentType: "application/json; charset=utf-8",
+        data: processId,
+        success: function (result) {
+            $("#existingComments").html(result);
+        },
+        error: function (error) {
+            $("#AddCommentError")
+                .html("<div class=\"alert alert-danger\" role=\"alert\">Failed to load comments.</div>");
+        }
+    });
+}
