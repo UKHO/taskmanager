269054fe4b6830210d7b3a1d6235338637d89072
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-11-26 18:21:42 +0000

    WIP: Moved DataImpact code to the partial; start using HdpUsage table to populate DataImpact

--- src/Portal/Portal/Pages/DbAssessment/_DataImpact.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/_DataImpact.cshtml.cs
@@ -1,24 +1,74 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WorkflowDatabase.EF;
+using WorkflowDatabase.EF.Models;
 
 namespace Portal.Pages.DbAssessment
 {
     public class _DataImpactModel : PageModel
     {
-        [DisplayName("Usage")]
-        public string Usage { get; set; }
+        private readonly WorkflowDbContext _dbContext;
 
-        [DisplayName("Edited")]
-        public string Edited { get; set; }
-        public SelectList ImpactedProducts { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int ProcessId { get; set; }
 
-        [DisplayName("Comments")]
-        public string Comments { get; set; }
+        public List<DataImpact> DataImpacts { get; set; }
 
+        public SelectList Usages { get; set; }
 
-        public void OnGet()
+        public _DataImpactModel(WorkflowDbContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public async Task OnGetAsync(int processId)
+        {
+            ProcessId = processId;
+
+            await PopulateUsages();
+            await SetDataImpactModelDummyData();
+        }
+
+        private async Task PopulateUsages()
+        {
+            var usages = await _dbContext.HpdUsage.ToListAsync();
+            Usages = new SelectList(usages, nameof(HpdUsage.HpdUsageId), nameof(HpdUsage.Name));
+        }
+
+        private async Task SetDataImpactModelDummyData()
+        {
+            var usages = await _dbContext.HpdUsage.ToListAsync();
+
+            DataImpacts = new List<DataImpact>
+            {
+                new DataImpact()
+                {
+                    DataImpactId = 1,
+                    Comments = "Test1", 
+                    HpdUsageId = 1,  
+                    HpdUsage = usages.FirstOrDefault(h =>h.HpdUsageId == 1), 
+                    Edited = false,
+                    ProcessId = ProcessId, 
+                    Verified = false
+                },
+
+                new DataImpact()
+                {
+                    DataImpactId = 2,
+                    Comments = "Test2",
+                    HpdUsageId = 2,
+                    HpdUsage = usages.FirstOrDefault(h =>h.HpdUsageId == 2),
+                    Edited = false,
+                    ProcessId = ProcessId,
+                    Verified = false
+                }
+            };
         }
     }
 }
--- src/Portal/Portal/wwwroot/js/DataImpact.js
+++ src/Portal/Portal/wwwroot/js/DataImpact.js
@@ -1,8 +1,37 @@
 ﻿$(document).ready(function () {
+    getDataImpacts();
+});
+
+function getDataImpacts() {
+    var processId = { "processId": Number($("#hdnProcessId").val()) };
+
+    $.ajax({
+        type: "GET",
+        url: "_DataImpact",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        contentType: "application/json; charset=utf-8",
+        data: processId,
+        success: function (result) {
+            $("#existingDataImpacts").html(result);
+
+            setVerified();
+        },
+        error: function (error) {
+            $("#AddDataImpactsError")
+                .html("<div class=\"alert alert-danger\" role=\"alert\">Failed to load Data Impacts.</div>");
+        }
+    });
+}
+
+
+function setVerified() {
+
     var pageIdentity = $("#pageIdentity").val();
     if (pageIdentity === "Assess") {
         $(".usageVerified").prop("disabled", true);
     } else if (pageIdentity === "Verify") {
         $(".usageVerified").prop("disabled", false);
     }
-});
+}
