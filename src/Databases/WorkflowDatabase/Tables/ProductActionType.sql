0709f434365d9d9a552576ae2a21580987445914
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-12-05 16:28:55 +0000

    Added [ProductActionType] lookup table

---
+++ src/Databases/WorkflowDatabase/Tables/ProductActionType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ProductActionType]
+(
+	[ProductActionTypeId] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(255) NOT NULL, 
+    CONSTRAINT [AK_ProductActionType_Name] UNIQUE ([Name])
+)
