dacb22074fb7d40493c247e665dc83b297857f8d
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-12-05 17:27:44 +0000

    Added [DbAssessmentAssessData]

---
+++ src/Databases/WorkflowDatabase/Tables/DbAssessmentAssessData.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[DbAssessmentAssessData]
+(
+	[DbAssessmentAssessDataId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProcessId] INT NOT NULL, 
+    [Ion] NVARCHAR(50) NULL, 
+    [ActivityCode] NVARCHAR(50) NULL, 
+    [WorkManager] NVARCHAR(255) NULL, 
+    [Assessor] NVARCHAR(255) NULL, 
+    [Verifier] NVARCHAR(255) NULL, 
+    [TaskComplexity] NVARCHAR(50) NULL, 
+    [WorkflowInstanceId] INT NOT NULL, 
+    [ProductActioned] BIT NULL, 
+    [ProductActionChangeDetails] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_DbAssessmentAssessData_WorkflowInstance] FOREIGN KEY ([WorkflowInstanceId]) REFERENCES [WorkflowInstance]([WorkflowInstanceId])
+)
