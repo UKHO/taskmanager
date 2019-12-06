86e661ed0776dcd185619497d27b5d34f2938176
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-12-06 10:28:46 +0000

    Added ProductAction table
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ src/Databases/WorkflowDatabase/Tables/ProductAction.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProductAction]
+(
+	[ProductActionId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProcessId] INT NOT NULL, 
+    [ImpactedProduct] NVARCHAR(100) NOT NULL, 
+    [ProductActionTypeId] INT NOT NULL, 
+    [Verified] BIT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_ProductAction_ProductActionType] FOREIGN KEY ([ProductActionTypeId]) REFERENCES [ProductActionType]([ProductActionTypeId]), 
+    CONSTRAINT [FK_ProductAction_WorkflowInstance] FOREIGN KEY ([ProcessId]) REFERENCES [WorkflowInstance]([ProcessId])
+)
