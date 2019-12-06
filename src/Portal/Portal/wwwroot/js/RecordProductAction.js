436ed70f9e7f10c1ae4bd1f61efd9a9bc11e5c49
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2019-12-06 20:37:42 +0000

    Add missing closing brace

--- src/Portal/Portal/wwwroot/js/RecordProductAction.js
+++ src/Portal/Portal/wwwroot/js/RecordProductAction.js
@@ -25,62 +25,63 @@
                     .html("<div class=\"alert alert-danger\" role=\"alert\">Failed to load Product Actions.</div>");
             }
         });
+    }
 
 
-        function update() {
+    function update() {
 
-            $(".recordProductAction").each(function (index, element) {
-                if (index > 0) {
-                    $(element).find(".deleteAction").show();
-                } else {
-                    $(element).find(".deleteAction").hide();
-                }
-                //Set Form Control Names
-                //$(element).find($(".assignTaskAssessor")).prop("name", "AssignTaskModel[" + index + "].Assessor.AssessorId");
-                //$(element).find($(".assignTaskVerifier")).prop("name", "AssignTaskModel[" + index + "].Verifier.VerifierId");
-                //$(element).find($(".assignTaskSourceType")).prop("name", "AssignTaskModel[" + index + "].SourceType.SourceTypeId");
-                //$(element).find($(".assignTaskWorkspaceAffected")).prop("name", "AssignTaskModel[" + index + "].WorkspaceAffected");
-                //$(element).find($(".assignTaskNotes")).prop("name", "AssignTaskModel[" + index + "].Notes");
+        $(".recordProductAction").each(function (index, element) {
+            if (index > 0) {
+                $(element).find(".deleteAction").show();
+            } else {
+                $(element).find(".deleteAction").hide();
+            }
+            //Set Form Control Names
+            //$(element).find($(".assignTaskAssessor")).prop("name", "AssignTaskModel[" + index + "].Assessor.AssessorId");
+            //$(element).find($(".assignTaskVerifier")).prop("name", "AssignTaskModel[" + index + "].Verifier.VerifierId");
+            //$(element).find($(".assignTaskSourceType")).prop("name", "AssignTaskModel[" + index + "].SourceType.SourceTypeId");
+            //$(element).find($(".assignTaskWorkspaceAffected")).prop("name", "AssignTaskModel[" + index + "].WorkspaceAffected");
+            //$(element).find($(".assignTaskNotes")).prop("name", "AssignTaskModel[" + index + "].Notes");
 
-                //Set Heading
-                //$(element).find("span").text("Assign Task " + (index + 1));
-                if (index > 0) {
-                    setDeleteHandler($(element).find(".deleteAction"));
-                }
+            //Set Heading
+            //$(element).find("span").text("Assign Task " + (index + 1));
+            if (index > 0) {
+                setDeleteHandler($(element).find(".deleteAction"));
+            }
 
-                var pageIdentity = $("#pageIdentity").val();
-                if (pageIdentity === "Assess") {
-                    $(".verifiedProduct").prop("disabled", true);
-                } else if (pageIdentity === "Verify") {
-                    $(".verifiedProduct").prop("disabled", false);
-                }
+            var pageIdentity = $("#pageIdentity").val();
+            if (pageIdentity === "Assess") {
+                $(".verifiedProduct").prop("disabled", true);
+            } else if (pageIdentity === "Verify") {
+                $(".verifiedProduct").prop("disabled", false);
+            }
 
-            });
-        };
+        });
+    };
 
-        function setCreateHandler() {
-            $("#btnAddImpact").on("click", function (e) {
-                id += 1; // TODO: Temporary solution for ADDED Record Product Action
-                var currentCount = $(".recordProductAction").length;
-                var newThing = $($(".recordProductAction")[0]).clone();
+    function setCreateHandler() {
+        $("#btnAddImpact").on("click", function (e) {
+            id += 1; // TODO: Temporary solution for ADDED Record Product Action
+            var currentCount = $(".recordProductAction").length;
+            var newThing = $($(".recordProductAction")[0]).clone();
 
-                $(newThing).find(".impactedProduct").val(0);
-                $(newThing).find(".productActionType").val(0);
-                $(newThing).find(".verifiedProduct").attr('id', 'newImpact-' + id); // TODO: Temporary solution for ADDED Record Product Action
-                $(newThing).find(".verifiedProductLabel").attr('for', 'newImpact-' + id); // TODO: Temporary solution for ADDED Record Product Action
+            $(newThing).find(".impactedProduct").val(0);
+            $(newThing).find(".productActionType").val(0);
+            $(newThing).find(".verifiedProduct").attr('id', 'newImpact-' + id); // TODO: Temporary solution for ADDED Record Product Action
+            $(newThing).find(".verifiedProductLabel").attr('for', 'newImpact-' + id); // TODO: Temporary solution for ADDED Record Product Action
 
-                $("#productActions").append(newThing);
-                $(newThing).show();
+            $("#productActions").append(newThing);
+            $(newThing).show();
 
-                update();
-            });
-        }
+            update();
+        });
+    }
 
-        function setDeleteHandler(element) {
-            $(element).on("click").click(function () {
-                $(element).parents(".recordProductAction").remove();
+    function setDeleteHandler(element) {
+        $(element).on("click").click(function () {
+            $(element).parents(".recordProductAction").remove();
 
-                update();
-            }).show();
-        }
-    });
+            update();
+        }).show();
+    }
+});
