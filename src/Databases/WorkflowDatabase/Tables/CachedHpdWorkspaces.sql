66671e496992c730eff389ac3a0b85e3ec3ff00f
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2019-12-10 12:12:53 +0000

    Added new table to store cached Caris workspaces
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
    Co-authored-by: Ben Ilo <42581204+247benji@users.noreply.github.com>

---
+++ src/Databases/WorkflowDatabase/Tables/CachedHpdWorkspaces.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CachedHpdWorkspaces]
+(
+	[CachedHpdWorkspacesId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_CachedHpdWorkspaces_Name] ON [dbo].[CachedHpdWorkspaces] ([Name])
