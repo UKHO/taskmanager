8bb55ab7128b8ef77c68d61b7bd6d6f3927c8cea
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-01-07 13:53:27 +0000

    Updated DataService Startup Secret Config with Sql DB account

--- src/DataServices/DataServices/Config/StartupSecretsConfig.cs
+++ src/DataServices/DataServices/Config/StartupSecretsConfig.cs
@@ -5,5 +5,7 @@
         public string DataSource { get; set; }
         public string UserId { get; set; }
         public string Password { get; set; }
+        public string SqlLoggingUsername { get; set; }
+        public string SqlLoggingPassword { get; set; }
     }
 }
