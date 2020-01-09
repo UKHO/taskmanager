0a8e769058490725e57ddd4e5032ddfa50cfe3f1
Rajan Shunmuga <23729848+ssrajan@users.noreply.github.com>
2020-01-09 13:54:55 +0000

    ncne Data structure creation - WIP

---
+++ src/Databases/NCNEWorkflowDatabase/Tables/NcneTaskInfo.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[NcneTaskInfo]
+(
+	[ProcessId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Ion] NVARCHAR(50) NULL, 
+    [ChartNumber] INT NULL, 
+    [Country] NVARCHAR(75) NULL, 
+    [ChartType] NVARCHAR(10) NULL, 
+    [WorkflowType] NVARCHAR(10) NULL, 
+    [Dating] NVARCHAR(10) NULL, 
+    [PublicationDate] DATETIME NULL, 
+    [AnnounceDate] DATETIME NULL, 
+    [CommitDate] DATETIME NULL, 
+    [CISDate] DATETIME NULL, 
+    [Compiler] NVARCHAR(50) NULL, 
+    [VerifierOne] NVARCHAR(50) NULL, 
+    [VerifierTwo] NVARCHAR(50) NULL, 
+    [Publisher] NVARCHAR(50) NULL
+)
