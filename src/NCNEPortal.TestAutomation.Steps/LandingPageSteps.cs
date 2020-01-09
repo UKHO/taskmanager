e4c01f1a0b73aec69f6765bec3cb6d43b029ee92
Gareth Evans <51328957+gareth72evans@users.noreply.github.com>
2020-01-09 11:07:25 +0000

    Added Specflow and linked Steps to Specs Project

---
+++ src/NCNEPortal.TestAutomation.Steps/LandingPageSteps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace NCNEPortal.TestAutomation.Steps
+{
+    [Binding]
+    public sealed class LandingPageSteps
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext context;
+
+        public LandingPageSteps(ScenarioContext injectedContext)
+        {
+            context = injectedContext;
+        }
+
+    }
+}
