5cbc73ba1b02b0b21cea18250383a1e0f67329d2
Gareth Evans <51328957+gareth72evans@users.noreply.github.com>
2020-01-09 10:55:30 +0000

    Added NCENEPortal Test Framework Project

---
+++ src/NCNEPortal.TestAutomation.Steps/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NCNEPortal.TestAutomation.Steps
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
