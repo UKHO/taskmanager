eac6922b1e204873608148a2e294dc216dc77695
247benji <42581204+247benji@users.noreply.github.com>
2020-01-10 16:00:12 +0000

    WIP: Changed btnDone type from "submit" to "button" on assess page. Added handlers for done button. Added function to assess.js to complete assess stage.
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- src/Portal/Portal/wwwroot/js/Assess.js
+++ src/Portal/Portal/wwwroot/js/Assess.js
@@ -1,19 +1,86 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
+
     setAssessDoneHandler();
+    setAssessSaveHandler();
+
+    var formChanged = false;
+    $("#frmAssessPage").change(function () { formChanged = true; });
+
+    window.onbeforeunload = function () {
+        if (formChanged) {
+            return "Changes detected";
+        }
+    };
 
     if ($("#assessDoneErrorMessage").html().trim().length > 0) {
         $("#modalWaitAssessDoneErrors").modal("show");
     }
-});
 
-function setAssessDoneHandler() {
-    $("#btnDone").prop("disabled", false);
-
-    $("#assessDone").on("submit", function (e) {
+    function completeAssess(action) {
+        $("#assessDoneErrorMessage").html("");
         $("#btnDone").prop("disabled", true);
+        $("#btnSave").prop("disabled", true);
         $("#modalWaitAssessDone").modal("show");
 
-        //e.preventDefault();
+        var formData = $('#frmAssessPage').serialize();
 
-    });
-}
+        $.ajax({
+            type: "POST",
+            url: "Assess/?handler=Done&action=" + action,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("RequestVerificationToken", $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: formData,
+            complete: function () {
+                //Add a delay to account for the modalWaitReviewDone modal
+                //not being fully shown, before trying to hide it
+                window.setTimeout(function () {
+                    $("#modalWaitAssessDone").modal("hide");
+                    $("#btnDone").prop("disabled", false);
+                    $("#btnSave").prop("disabled", false);
+                }, 200);
+            },
+            success: function (result) {
+                formChanged = false;
+                if (action === "Done") {
+                    window.location.replace("/Index");
+                }
+                console.log("success");
+            },
+            error: function (error) {
+                var responseJson = error.responseJSON;
+
+                if (responseJson != null) {
+                    $("#assessDoneErrorMessage").append("<ul/>");
+                    var unOrderedList = $("#assessDoneErrorMessage ul");
+
+                    responseJson.forEach(function (item) {
+                        unOrderedList.append("<li>" + item + "</li>");
+                    });
+
+                    $("#modalWaitAssessDoneErrors").modal("show");
+                }
+
+            }
+        });
+    }
+
+    function setAssessDoneHandler() {
+        $("#btnDone").prop("disabled", false);
+
+
+        $("#btnDone").click(function (e) {
+            completeAssess("Done");
+        });
+    }
+
+    function setAssessSaveHandler() {
+        $("#btnSave").prop("disabled", false);
+
+
+        $("#btnSave").click(function (e) {
+            completeAssess("Save");
+        });
+    }
+
+});
