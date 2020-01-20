08e49d6a33fde0fee4ef517510b09471d3ab89b9
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-01-20 16:55:03 +0000

    Created new event: PersistWorkflowInstanceDataEvent
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ src/Common/Common.Messages/Events/PersistWorkflowInstanceDataEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common.Messages.Events
+{
+    public class PersistWorkflowInstanceDataEvent : ICorrelate
+    {
+        public Guid CorrelationId { get; set; }
+        public int ProcessId { get; set; }
+        public string FromActivityName { get; set; }
+        public string ToActivityName { get; set; }
+    }   
+}
