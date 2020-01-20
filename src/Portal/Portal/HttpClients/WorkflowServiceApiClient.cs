c3a2f290888dadb4e2dd9c3f1da1fa61f275f7f9
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-01-20 14:55:28 +0000

    Added logging
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- src/Portal/Portal/HttpClients/WorkflowServiceApiClient.cs
+++ src/Portal/Portal/HttpClients/WorkflowServiceApiClient.cs
@@ -3,21 +3,29 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Portal.Configuration;
 using Portal.Models;
+using Portal.Pages.DbAssessment;
+using Serilog.Context;
 
 namespace Portal.HttpClients
 {
     public class WorkflowServiceApiClient : IWorkflowServiceApiClient
     {
         private readonly IOptions<UriConfig> _uriConfig;
+        private readonly ILogger<WorkflowServiceApiClient> _logger;
         private readonly HttpClient _httpClient;
 
-        public WorkflowServiceApiClient(HttpClient httpClient, IOptions<GeneralConfig> generalConfig, IOptions<UriConfig> uriConfig)
+        public WorkflowServiceApiClient(
+                                        HttpClient httpClient,
+                                        IOptions<UriConfig> uriConfig,
+                                        ILogger<WorkflowServiceApiClient> logger)
         {
             _uriConfig = uriConfig;
+            _logger = logger;
             _httpClient = httpClient;
         }
 
@@ -80,13 +88,39 @@ namespace Portal.HttpClients
 
         public async Task<bool> ProgressWorkflowInstance(int processId,string serialNumber, string currentTaskStage, string progressToTaskStage)
         {
+            LogContext.PushProperty("ProcessId", processId);
+            LogContext.PushProperty("SerialNumber", serialNumber);
+            LogContext.PushProperty("ActivityName", currentTaskStage);
+            LogContext.PushProperty("ProgressToTaskStage", progressToTaskStage);
+            LogContext.PushProperty("PortalResource", nameof(ProgressWorkflowInstance));
+
+            _logger.LogInformation("Entering {PortalResource} with: ProcessId: {ProcessId}; "
+                                            + "SerialNumber: {SerialNumber}; "
+                                            + "CurrentTaskStage: {ActivityName}; "
+                                            + "ProgressToTaskStage: {ProgressToTaskStage}");
+
             var taskStageInK2 = await GetTaskCurrentStage(serialNumber);
 
+            LogContext.PushProperty("TaskStageInK2", taskStageInK2);
+
             if (taskStageInK2.Equals(progressToTaskStage))
+            {
+                _logger.LogInformation("{PortalResource} with: ProcessId: {ProcessId}; "
+                                       + "SerialNumber: {SerialNumber}; "
+                                       + "is already at the TaskStageInK2: {TaskStageInK2}");
+
                 return true;
+            }
 
             if (!taskStageInK2.Equals(currentTaskStage))
-                throw new ApplicationException($"K2 task with Process Id {processId} and Serial Number {serialNumber} is not at the expected stage {currentTaskStage} but instead its at {taskStageInK2}");
+            {
+                _logger.LogInformation("{PortalResource} with: ProcessId: {ProcessId}; "
+                                       + "SerialNumber: {SerialNumber}; "
+                                       + "is at an unexpected stage in K2 of the TaskStageInK2: {TaskStageInK2}");
+
+                throw new ApplicationException(
+                    $"K2 task with Process Id {processId} and Serial Number {serialNumber} is not at the expected stage {currentTaskStage} but instead its at {taskStageInK2}");
+            }
 
             var fullUri = new Uri(_uriConfig.Value.K2WebServiceBaseUri, _uriConfig.Value.K2WebServiceGetTasksUri + $"{serialNumber}/actions/Approve");
 
@@ -99,6 +133,11 @@ namespace Portal.HttpClients
                                                    $"\n Message= '{data}'," +
                                                    $"\n Url='{fullUri}'");
             }
+
+            _logger.LogInformation("{PortalResource} with: ProcessId: {ProcessId}; "
+                                   + "SerialNumber: {SerialNumber}; "
+                                   + "successfully progressed the task to ProgressToTaskStage: {ProgressToTaskStage}");
+
 
             return true;
         }
