f70bd36002c335243f9bae43d3a1c17503f616d7
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-01-22 17:11:57 +0000

    Added a wait for K2 task to become ready after progress

--- src/WorkflowCoordinator.IntegrationTests/EndToEndK2WorkflowTests/WorkflowEndToEndTests.cs
+++ src/WorkflowCoordinator.IntegrationTests/EndToEndK2WorkflowTests/WorkflowEndToEndTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
@@ -43,6 +44,11 @@ namespace WorkflowCoordinator.IntegrationTests.EndToEndK2WorkflowTests
             var processId = await _workflowServiceApiClient.CreateWorkflowInstance(workflowId);
             var serialNumber = await _workflowServiceApiClient.GetWorkflowInstanceSerialNumber(processId);
 
+            if (string.IsNullOrEmpty(serialNumber))
+            {
+                throw new ApplicationException($"Failed to get data for K2 Task with ProcessId {processId}");
+            }
+
             serialNumber = await ProgressAndValidate(processId, serialNumber, "Review", "Assess");
             serialNumber = await ProgressAndValidate(processId, serialNumber, "Assess", "Verify");
             await ProgressAndValidate(processId, serialNumber, "Verify", "Complete");
@@ -52,11 +58,29 @@ namespace WorkflowCoordinator.IntegrationTests.EndToEndK2WorkflowTests
         {
             var success = await _workflowServiceApiClient.ProgressWorkflowInstance(processId, serialNumber);
             Assert.IsTrue(success, $"Task has not moved from {fromTask} to {toTask}.");
-            
-            var k2Task = await _workflowServiceApiClient.GetWorkflowInstanceData(processId);
-            Assert.IsTrue(k2Task.ActivityName.Equals(toTask, StringComparison.OrdinalIgnoreCase));
 
-            return k2Task.SerialNumber;
+            var count = 0;
+            var newSerialNumber = "";
+
+            if (!toTask.Equals("Complete", StringComparison.OrdinalIgnoreCase))
+            {
+                K2TaskData k2Task = null;
+                // there is a delay between progressing task in K2 and when it is ready; hence added this do--lopp
+                do
+                {
+                    k2Task = await _workflowServiceApiClient.GetWorkflowInstanceData(processId);
+                    count++;
+                } while (k2Task == null && count < 5);
+
+                Console.WriteLine($"count: {count}");
+
+                Assert.IsNotNull(k2Task);
+                Assert.IsTrue(k2Task.ActivityName.Equals(toTask, StringComparison.OrdinalIgnoreCase));
+
+                newSerialNumber = k2Task.SerialNumber;
+            }
+
+            return newSerialNumber;
         }
 
         private WorkflowServiceApiClient SetupWorkflowServiceApiClient(StartupSecretsConfig startupSecretsConfig,
