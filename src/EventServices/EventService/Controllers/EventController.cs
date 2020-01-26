e6ae8c2f37cec88c3585762d331bbf603cb69fc1
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-01-26 12:25:04 +0000

    Add retry logic to EventService NSB Publish.
    - Used Polly package
    - Returning 503 on failure to clarify to caller that service is idempotent and it is safe for them to retry the POST.
    - To keep REST response reasonable have (initially hardcoded) 3 retries at 2, 4 and 6 seconds, which should be enough to cover any transient faults with the database.
    
    Need to keep service idempotent and respond to caller in a reasonable time to retry. If caller does not automate retries against the API then they when the errror surfaces we can replay the event firing via Swagger as the JSON is already logged.
    
    If we decide that caller can fire and forget without any knowledge of error then we can build more into this retry.

--- src/EventServices/EventService/Controllers/EventController.cs
+++ src/EventServices/EventService/Controllers/EventController.cs
@@ -11,6 +11,7 @@ using EventService.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
+using Polly;
 using Serilog.Context;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -73,7 +74,7 @@ namespace EventService.Controllers
 
             //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
             // return StatusCode(500, default(DefaultErrorResponse));
-           
+
             throw new NotImplementedException();
         }
 
@@ -161,7 +162,7 @@ namespace EventService.Controllers
 
             try
             {
-                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace(@"file:\","");
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace(@"file:\", "");
                 var assemblyPath = Path.Combine(path, "Common.Messages.dll");
                 assembly = Assembly.LoadFrom(assemblyPath);
             }
@@ -194,7 +195,7 @@ namespace EventService.Controllers
 
             try
             {
-                var correlationId = ((ICorrelate) populatedEvent).CorrelationId;
+                var correlationId = ((ICorrelate)populatedEvent).CorrelationId;
 
                 if (correlationId.Equals(Guid.Empty))
                 {
@@ -214,19 +215,24 @@ namespace EventService.Controllers
                 return StatusCode(500, $"Failed to interpret CorrelationId from {eventName}: {e.ToString()}");
             }
 
-            try
-            {
-                var publishOptions = new PublishOptions();
+            _logger.LogInformation("{ApiResource} publishing event {EventName}");
 
-                _logger.LogInformation("{ApiResource} publishing event {EventName}");
-                await _messageSession.Publish(populatedEvent, publishOptions).ConfigureAwait(false);
-                _logger.LogInformation("{ApiResource} finished publishing event {EventName}");
-            }
-            catch (Exception e)
+            // Retries 3 times at 2, 4 and 6 seconds
+            var result = await Policy.Handle<Exception>()
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt * 2),
+                    (exception, timeSpan, retryAttempt, context) =>
+                    {
+                        _logger.LogError(exception, $"Failed to publish event {eventName}, attempting retry: {retryAttempt}");
+                    })
+                .ExecuteAndCaptureAsync(() => _messageSession.Publish(populatedEvent, new PublishOptions()));
+
+            if (result.Outcome == OutcomeType.Failure)
             {
-                _logger.LogError(e, "{ApiResource} Failed to publish event {EventName}");
-                return StatusCode(500, $"Failed to publish event {eventName}: {e.ToString()}");
+                _logger.LogError(result.FinalException, "{ApiResource} Failed to publish event {EventName}");
+                return StatusCode(503, $"Failed to publish event {eventName}. Please try again later.");
             }
+
+            _logger.LogInformation("{ApiResource} finished publishing event {EventName}");
 
             return new ObjectResult(HttpStatusCode.OK);
         }
