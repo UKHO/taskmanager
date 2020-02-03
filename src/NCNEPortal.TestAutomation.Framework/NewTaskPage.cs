000f7fe4c5f8366842b972d3afa3890b80956a8e
Gareth Evans <51328957+gareth72evans@users.noreply.github.com>
2020-02-03 15:33:51 +0000

    Added NewTaskPage.Config.cs

---
+++ src/NCNEPortal.TestAutomation.Framework/NewTaskPage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Common.Helpers;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+namespace NCNEPortal.TestAutomation.Framework
+{
+    public class NewTaskPage
+
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+        private const int SeleniumTimeoutSeconds = 5;
+        private readonly Uri _workflowPageUrl;
+        private readonly WorkflowPageConfig _config = new WorkflowPageConfig();
+
+        private IWebElement UkhoLogo => _driver.FindElement(By.Id("ukhoLogo"));
+
+        public NewTaskPage(IWebDriver driver)
+        {
+            var configRoot = AzureAppConfigConfigurationRoot.Instance;
+            configRoot.GetSection("urls").Bind(_config);
+
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(SeleniumTimeoutSeconds));
+
+            _workflowPageUrl = ConfigHelpers.IsAzureDevOpsBuild
+                ? _config.NcneWorkflowPageUrl
+                : _config.NcneLocalDevWorkflowPageUrl;
+        }
+
+        public void NavigateTo()
+        {
+            _driver.Navigate().GoToUrl(_workflowPageUrl);
+            _driver.Manage().Window.Maximize();
+        }
+
+        public bool HasLoaded()
+        {
+            try
+            {
+                _wait.Until(driver => UkhoLogo.Displayed);
+                return true;
+            }
+            catch (NoSuchElementException e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+        }
+    }
+}
--- src/NCNEPortal.TestAutomation.Steps/NewTaskPageSteps.cs
+++ src/NCNEPortal.TestAutomation.Steps/NewTaskPageSteps.cs
@@ -6,25 +6,30 @@ namespace NCNEPortal.TestAutomation.Steps
 
     [Binding]
     public sealed class NewTaskPageSteps
-    
-    {
 
+    {
+        private readonly NewTaskPage _newtaskPage;
+        public NewTaskPageSteps(NewTaskPage newtaskPage)
+
+        {
+            _newtaskPage = newtaskPage;
+        }
+
+        
         [Given(@"I navigate to the NCNE New Task page")]
         public void GivenINavigateToTheNCNENewTaskPage()
+
         {
-            ScenarioContext.Current.Pending();
+            _newtaskPage.NavigateTo();
         }
 
-
-
+        
         [Then(@"The NCNE New Task page has loaded")]
         public void ThenTheNCNENewTaskPageHasLoaded()
+
         {
-            ScenarioContext.Current.Pending();
+            _newtaskPage.HasLoaded();
         }
-
-
-
     }
 
 
