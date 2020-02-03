c76a820246255137433d4fafc19af28c6d0c572c
Gareth Evans <51328957+gareth72evans@users.noreply.github.com>
2020-02-03 14:44:19 +0000

    NewTaskPage feature file and NewTaskPage Steps.cs added

---
+++ src/NCNEPortal.TestAutomation.Steps/NewTaskPageSteps.cs
@@ -0,0 +1,31 @@
+﻿using NCNEPortal.TestAutomation.Framework;
+using TechTalk.SpecFlow;
+
+namespace NCNEPortal.TestAutomation.Steps
+{
+
+    [Binding]
+    public sealed class NewTaskPageSteps
+    
+    {
+
+        [Given(@"I navigate to the NCNE New Task page")]
+        public void GivenINavigateToTheNCNENewTaskPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+
+        [Then(@"The NCNE New Task page has loaded")]
+        public void ThenTheNCNENewTaskPageHasLoaded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+
+    }
+
+
+}
