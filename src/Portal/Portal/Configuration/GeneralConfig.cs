ee4c508e5bab72d97886513278fe2c0f823568c7
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-02-05 15:43:28 +0000

    Create new app config entry for Simple and LTA
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>
    Co-authored-by: Benjamin Ilo <42581204+247benji@users.noreply.github.com>

--- src/Portal/Portal/Configuration/GeneralConfig.cs
+++ src/Portal/Portal/Configuration/GeneralConfig.cs
@@ -6,7 +6,8 @@ namespace Portal.Configuration
         public string K2DBAssessmentWorkflowName { get; set; }
         public string AzureAdClientId { get; set; }
         public string TenantId { get; set; }
-        public int DmEndDateDays { get; set; }
+        public int DmEndDateDaysSimple { get; set; }
+        public int DmEndDateDaysLTA { get; set; }
         public int DaysToDmEndDateRedAlertUpperInc { get; set; }
         public int DaysToDmEndDateAmberAlertUpperInc { get; set; }
         public string SessionFilename { get; set; }
