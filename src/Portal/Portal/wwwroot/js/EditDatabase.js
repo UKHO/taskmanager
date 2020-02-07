7f75956919247cd8e8ca1222d2c44ac6f207964f
247benji <42581204+247benji@users.noreply.github.com>
2020-02-07 12:21:20 +0000

    Added Caris data to _EditDatabase partial.
    Co-authored-by: Ross Sandford <12933990+Rossall82@users.noreply.github.com>
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>

--- src/Portal/Portal/wwwroot/js/EditDatabase.js
+++ src/Portal/Portal/wwwroot/js/EditDatabase.js
@@ -4,7 +4,8 @@
 
 function getEditDatabase() {
 
-    var processId = { "processId": Number($("#hdnProcessId").val()) };
+    var processId = Number($("#hdnProcessId").val());
+    var pageIdentity = $("#pageIdentity").val();
 
     $.ajax({
         type: "GET",
@@ -13,7 +14,10 @@ function getEditDatabase() {
             xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
         },
         contentType: "application/json; charset=utf-8",
-        data: processId,
+        data: {
+            "processId": processId,
+            "taskStage": pageIdentity
+        }, 
         success: function (result) {
             $("#editDatabase").html(result);
             launchSourceEditorDownloadHandler();
