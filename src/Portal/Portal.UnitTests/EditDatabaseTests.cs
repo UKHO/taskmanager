132b0dea7780e1bfb818681b54328d7a0b5237d7
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-02-18 09:03:07 +0000

    Add unit tests for validating CreateCarisProject step

---
+++ src/Portal/Portal.UnitTests/EditDatabaseTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Helpers;
+using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using Portal.Auth;
+using Portal.Configuration;
+using Portal.Helpers;
+using Portal.Pages.DbAssessment;
+using WorkflowDatabase.EF;
+using WorkflowDatabase.EF.Models;
+
+namespace Portal.UnitTests
+{
+    public class EditDatabaseTests
+    {
+        private WorkflowDbContext _dbContext;
+        private _EditDatabaseModel _editDatabaseModel;
+        private ILogger<_EditDatabaseModel> _fakeLogger;
+        private IOptions<GeneralConfig> _generalConfig;
+        private IUserIdentityService _fakeUserIdentityService;
+        private ISessionFileGenerator _fakeSessionFileGenerator;
+        private ICarisProjectHelper _fakeCarisProjectHelper;
+        public int ProcessId { get; set; }
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<WorkflowDbContext>()
+                .UseInMemoryDatabase(databaseName: "inmemory")
+                .Options;
+
+            _dbContext = new WorkflowDbContext(dbContextOptions);
+
+            _fakeLogger = A.Dummy<ILogger<_EditDatabaseModel>>();
+            _generalConfig = A.Fake<IOptions<GeneralConfig>>();
+            _fakeUserIdentityService = A.Fake<IUserIdentityService>();
+            _fakeSessionFileGenerator = A.Fake<ISessionFileGenerator>();
+            _fakeCarisProjectHelper = A.Fake<ICarisProjectHelper>();
+
+            ProcessId = 123;
+
+            _dbContext.CachedHpdWorkspace.Add(new CachedHpdWorkspace
+            {
+                Name = "TestWorkspace"
+            });
+            await _dbContext.SaveChangesAsync();
+
+            _editDatabaseModel = new _EditDatabaseModel(_dbContext, _fakeLogger, _generalConfig, _fakeUserIdentityService, _fakeSessionFileGenerator, _fakeCarisProjectHelper);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _dbContext.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public void Test_CreateCarisProject_Throws_InvalidOperationException_When_Invalid_Workspace_Provided()
+        {
+            Assert.ThrowsAsync<InvalidOperationException>(() =>
+                _editDatabaseModel.OnPostCreateCarisProjectAsync(ProcessId, "Assess", "RossTest", "InvalidWorkspace"));
+        }
+
+        [Test]
+        public void Test_CreateCarisProject_Throws_ArgumentException_When_No_ProjectName_Provided()
+        {
+            Assert.ThrowsAsync<ArgumentException>(() =>
+                _editDatabaseModel.OnPostCreateCarisProjectAsync(ProcessId, "Assess", null, "TestWorkspace"));
+        }
+    }
+}
