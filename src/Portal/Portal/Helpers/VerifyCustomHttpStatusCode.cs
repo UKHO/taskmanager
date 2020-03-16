63218b295bfd7d8e4811ff2f063283368b74bee1
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-03-16 17:40:27 +0000

    Added custom HttpStatusCodes for Verify page Save\Done\Signoff
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

---
+++ src/Portal/Portal/Helpers/VerifyCustomHttpStatusCode.cs
@@ -0,0 +1,9 @@
+﻿namespace Portal.Helpers
+{
+    public enum VerifyCustomHttpStatusCode
+    {
+        FailedValidation = 600,
+        FailuresDetected = 601,
+        WarningsDetected = 602
+    }
+}
