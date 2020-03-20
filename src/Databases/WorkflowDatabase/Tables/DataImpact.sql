389c5face8822187d014b95132e9e6c5506b53eb
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-03-20 14:42:00 +0000

    Updated DataImpact table with FeaturesVerified and FeaturesSubmitted

--- src/Databases/WorkflowDatabase/Tables/DataImpact.sql
+++ src/Databases/WorkflowDatabase/Tables/DataImpact.sql
@@ -5,7 +5,8 @@
     [HpdUsageId] INT NOT NULL,
     [Edited] BIT NOT NULL, 
 	[Comments] NVARCHAR(4000),
-	[Verified] BIT NOT NULL,
+	[FeaturesVerified] BIT NOT NULL,
+    [FeaturesSubmitted] BIT NOT NULL, 
     CONSTRAINT [FK_DataImpact_WorkflowInstance] FOREIGN KEY ([ProcessId]) REFERENCES [WorkflowInstance]([ProcessId]),
 	CONSTRAINT [FK_DataImpact_HpdUsage] FOREIGN KEY ([HpdUsageId]) REFERENCES [HpdUsage]([HpdUsageId])
 )
