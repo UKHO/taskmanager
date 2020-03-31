35f2998f10929a55583a89ff1d57955c01a99d5a
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-03-31 12:46:10 +0100

    Added handler for CompleteAssessmentCommand, fire the new command from PersistWorkflowInstanceDataEventHandler

---
+++ src/WorkflowCoordinator/WorkflowCoordinator/Handlers/CompleteAssessmentCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Common.Helpers;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using Serilog.Context;
+using WorkflowCoordinator.Messages;
+using WorkflowDatabase.EF;
+
+namespace WorkflowCoordinator.Handlers
+{
+    public class CompleteAssessmentCommandHandler : IHandleMessages<CompleteAssessmentCommand>
+    {
+        private readonly ILogger<CompleteAssessmentCommandHandler> _logger;
+        private readonly WorkflowDbContext _dbContext;
+
+        public CompleteAssessmentCommandHandler(ILogger<CompleteAssessmentCommandHandler> logger, WorkflowDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(CompleteAssessmentCommand message, IMessageHandlerContext context)
+        {
+            LogContext.PushProperty("MessageId", context.MessageId);
+            LogContext.PushProperty("Message", message.ToJSONSerializedString());
+            LogContext.PushProperty("EventName", nameof(CompleteAssessmentCommand));
+            LogContext.PushProperty("MessageCorrelationId", message.CorrelationId);
+            LogContext.PushProperty("ProcessId", message.ProcessId);
+
+            _logger.LogInformation("Entering {EventName} handler with: {Message}");
+
+        }
+    }
+}
