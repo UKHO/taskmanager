ca61eacb55d6422a024bf7f0419093ccf1ace4c6
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-03-31 16:47:10 +0100

    Added functionality for CompleteAssessmentCommand

--- src/WorkflowCoordinator/WorkflowCoordinator/Handlers/CompleteAssessmentCommandHandler.cs
+++ src/WorkflowCoordinator/WorkflowCoordinator/Handlers/CompleteAssessmentCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Common.Helpers;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 using Serilog.Context;
@@ -37,21 +39,55 @@ namespace WorkflowCoordinator.Handlers
 
             _logger.LogInformation("Entering {EventName} handler with: {Message}");
 
+            var workflowInstance = await _dbContext.WorkflowInstance
+                .Include(wi => wi.AssessmentData)
+                .Include(wi => wi.DbAssessmentVerifyData)
+                .FirstOrDefaultAsync(wi => wi.ProcessId == message.ProcessId);
+
+            var action =
+                workflowInstance.DbAssessmentVerifyData.TaskType.Equals("Simple",
+                    StringComparison.InvariantCultureIgnoreCase)
+                    ? "Imm Act - NM"
+                    : "Longer-term Action";
+            var sdocId = workflowInstance.AssessmentData.PrimarySdocId;
+
+            // TODO: Mark SDRA Assessment as Assessed first, then mark as completed
+
+            await UpdateSdraAssessmentAsAssessed(sdocId, message.ProcessId, action);
+            await UpdateSdraAssessmentAsCompleted(sdocId, action);
         }
 
-        private async Task UpdateSdraAssessmentAsCompleted(string comment, WorkflowInstance workflowInstance)
+        private async Task UpdateSdraAssessmentAsAssessed(int sdocId, int processId, string action)
+        {
+            // TODO: Mark SDRA Assessment as Assessed first, then mark as completed
+
+
+            try
+            {
+                //await _dataServiceApiClient.MarkAssessmentAsCompleted(workflowInstance.AssessmentData.PrimarySdocId,
+                //    comment);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e,
+                    "Failed requesting DataService {DataServiceResource} with: PrimarySdocId: {PrimarySdocId}",
+                    nameof(_dataServiceApiClient.MarkAssessmentAsCompleted),
+                    sdocId);
+            }
+        }
+
+        private async Task UpdateSdraAssessmentAsCompleted(int sdocId, string comment)
         {
             // TODO: Mark SDRA Assessment as Assessed first, then mark as completed
             try
             {
-                await _dataServiceApiClient.MarkAssessmentAsCompleted(workflowInstance.AssessmentData.PrimarySdocId,
-                    comment);
+                await _dataServiceApiClient.MarkAssessmentAsCompleted(sdocId, comment);
             }
             catch (Exception e)
             {
                 _logger.LogError(e, "Failed requesting DataService {DataServiceResource} with: PrimarySdocId: {PrimarySdocId}; Comment: {Comment};",
                     nameof(_dataServiceApiClient.MarkAssessmentAsCompleted),
-                    workflowInstance.AssessmentData.PrimarySdocId,
+                    sdocId,
                     comment);
             }
         }
