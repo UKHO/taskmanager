95ac90b1e2f014bfda9e6ff760733cebd8040ba2
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-03-31 17:55:38 +0100

    code tidy

--- src/Portal/Portal/HttpClients/DataServiceApiClient.cs
+++ src/Portal/Portal/HttpClients/DataServiceApiClient.cs
@@ -25,7 +25,7 @@ namespace Portal.HttpClients
         public async Task MarkAssessmentAsCompleted(int sdocId, string comment)
         {
             var data = "";
-            var fullUri = _uriConfig.Value.BuildDataServicesUri(_generalConfig.Value.CallerCode, sdocId, comment);
+            var fullUri = _uriConfig.Value.BuildDataServicesMarkAssessmentCompletedUri(_generalConfig.Value.CallerCode, sdocId, comment);
 
             using (var response = await _httpClient.PutAsync(fullUri.ToString(), null).ConfigureAwait(false))
             {
