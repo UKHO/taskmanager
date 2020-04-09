d8aa75729626133a677d571783cfd886360a3f2c
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-09 11:53:10 +0100

    Updated relevant DB tables with [Filename] and [Filepath]

--- src/Databases/WorkflowDatabase/Tables/AssessmentData.sql
+++ src/Databases/WorkflowDatabase/Tables/AssessmentData.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[AssessmentData]
 (
 			[AssessmentDataId] INT NOT NULL PRIMARY KEY IDENTITY,
+			[ProcessId] INT NOT NULL, 
 			[PrimarySdocId] INT NOT NULL,
 			[RsdraNumber] NVARCHAR(50) NOT NULL,
 			[SourceDocumentName] NVARCHAR(255) NOT NULL,
@@ -11,7 +12,8 @@
 			[SourceDocumentType] NVARCHAR(4000) NULL,
 			[SourceNature] NVARCHAR(255) NULL,
 			[Datum] NVARCHAR(2000) NULL, 
-			[ProcessId] INT NOT NULL, 
+			[Filename] NVARCHAR(100) NULL, 
+			[Filepath] NVARCHAR(255) NULL, 
     CONSTRAINT [AK_AssessmentData_ProcessId] UNIQUE ([ProcessId]), 
     CONSTRAINT [FK_AssessmentData_WorkflowInstance] FOREIGN KEY ([ProcessId]) REFERENCES [WorkflowInstance]([ProcessId])
 )
