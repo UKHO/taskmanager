e6035fb5edc5fd4e4997274fdccb182f5d773542
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-09 16:27:31 +0100

    Added saving the full filename of the generated source document when handling ClearDocumentRequestFromQueueCommand

--- src/SourceDocumentCoordinator/SourceDocumentCoordinator/Handlers/ClearDocumentRequestFromQueueCommandHandler.cs
+++ src/SourceDocumentCoordinator/SourceDocumentCoordinator/Handlers/ClearDocumentRequestFromQueueCommandHandler.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Common.Factories;
 using Common.Factories.Interfaces;
+using Common.Helpers;
+using Common.Messages.Events;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NServiceBus;
+using Serilog.Context;
 using SourceDocumentCoordinator.Config;
 using SourceDocumentCoordinator.Enums;
 using SourceDocumentCoordinator.HttpClients;
@@ -18,24 +23,46 @@ namespace SourceDocumentCoordinator.Handlers
         private readonly IDataServiceApiClient _dataServiceApiClient;
         private readonly IOptionsSnapshot<GeneralConfig> _generalConfig;
         private readonly IDocumentStatusFactory _documentStatusFactory;
+        private readonly ILogger<ClearDocumentRequestFromQueueCommandHandler> _logger;
 
         public ClearDocumentRequestFromQueueCommandHandler(IDataServiceApiClient dataServiceApiClient,
-            WorkflowDbContext dbContext, IOptionsSnapshot<GeneralConfig> generalConfig, IDocumentStatusFactory documentStatusFactory)
+                                                            WorkflowDbContext dbContext, 
+                                                            IOptionsSnapshot<GeneralConfig> generalConfig, 
+                                                            IDocumentStatusFactory documentStatusFactory, 
+                                                            ILogger<ClearDocumentRequestFromQueueCommandHandler> logger)
         {
             _dataServiceApiClient = dataServiceApiClient;
             _dbContext = dbContext;
             _generalConfig = generalConfig;
             _documentStatusFactory = documentStatusFactory;
+            _logger = logger;
         }
 
         public async Task Handle(ClearDocumentRequestFromQueueCommand message, IMessageHandlerContext context)
         {
+            LogContext.PushProperty("MessageId", context.MessageId);
+            LogContext.PushProperty("Message", message.ToJSONSerializedString());
+            LogContext.PushProperty("EventName", nameof(ClearDocumentRequestFromQueueCommand));
+            LogContext.PushProperty("MessageCorrelationId", message.CorrelationId);
+            LogContext.PushProperty("ProcessId", message.ProcessId);
+
+            _logger.LogInformation("Entering {EventName} handler with: {Message}");
+
             var returnCode = await _dataServiceApiClient
                 .DeleteDocumentRequestJobFromQueue(
                                                     _generalConfig.Value.CallerCode,
                                                     message.SourceDocumentId,
                                                     _generalConfig.Value.SourceDocumentWriteableFolderName)
                 .ConfigureAwait(false);
+
+            LogContext.PushProperty("ReturnCode", returnCode.ToJSONSerializedString());
+
+            if (string.IsNullOrWhiteSpace(returnCode.Message) || !Path.IsPathRooted(returnCode.Message.Trim()))
+            {
+                _logger.LogError("A call to DeleteDocumentRequestJobFromQueue did not return a path to the generated source document; ReturnCode: {ReturnCode}");
+
+                throw new ApplicationException($"A call to DeleteDocumentRequestJobFromQueue did not return a path to the generated source document; ReturnCode: {returnCode.ToJSONSerializedString()}");
+            }
 
             if (returnCode.Code.HasValue)
             {
@@ -48,7 +75,7 @@ namespace SourceDocumentCoordinator.Handlers
                                                     message.ProcessId, 
                                                     message.SourceDocumentId, 
                                                     SourceDocumentRetrievalStatus.FileGenerated, 
-                                                    message.SourceType, message.CorrelationId);
+                                                    message.SourceType, message.CorrelationId, generatedFullFilename:returnCode.Message.Trim());
                         break;
                     default:
                         throw new NotImplementedException();
