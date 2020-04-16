c80d139d82536b7eb23dbbe8feb87b36ad489f3f
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-16 16:40:13 +0100

    Added more properties to PersistDocumentInStoreCommand to be used for source document file location factory

--- src/SourceDocumentCoordinator/SourceDocumentCoordinator/Messages/PersistDocumentInStoreCommand.cs
+++ src/SourceDocumentCoordinator/SourceDocumentCoordinator/Messages/PersistDocumentInStoreCommand.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Common.Messages;
+using Common.Messages.Enums;
 
 namespace SourceDocumentCoordinator.Messages
 {
     public class PersistDocumentInStoreCommand : ICorrelate
     {
         public Guid CorrelationId { get; set; }
+        public int ProcessId { get; set; }
         public int SourceDocumentId { get; set; }
+        public SourceType SourceType { get; set; }
         public string Filepath { get; set; }
     }
 }
