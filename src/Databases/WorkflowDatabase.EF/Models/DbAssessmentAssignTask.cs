db2199fb85316fe7ea2efd07af23ac821cfb542b
247benji <42581204+247benji@users.noreply.github.com>
2020-04-16 14:56:42 +0100

    Added display names to models.

--- src/Databases/WorkflowDatabase.EF/Models/DbAssessmentAssignTask.cs
+++ src/Databases/WorkflowDatabase.EF/Models/DbAssessmentAssignTask.cs
@@ -6,12 +6,15 @@ namespace WorkflowDatabase.EF.Models
     {
         public int DbAssessmentAssignTaskId { get; set; }
         public int ProcessId { get; set; }
+        [DisplayName("Assessor:")]
         public string Assessor { get; set; }
+        [DisplayName("Verifier:")]
         public string Verifier { get; set; }
         [DisplayName("Task Type:")]
         public string TaskType { get; set; }
         [DisplayName("Workspace Affected:")]
         public string WorkspaceAffected { get; set; }
+        [DisplayName("Notes:")]
         public string Notes { get; set; }
     }
 }
