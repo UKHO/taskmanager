55c301031bb54f056170f4a0a01889c62aeda86f
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-16 16:37:16 +0100

    Added new factory for processing source document file location

---
+++ src/Common.Factories/DocumentFileLocationFactory/DocumentFileLocationFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Common.Factories.Interfaces;
+using Common.Messages.Enums;
+using WorkflowDatabase.EF;
+
+namespace Common.Factories.DocumentFileLocationFactory
+{
+    public class DocumentFileLocationFactory : IDocumentFileLocationFactory
+    {
+        private readonly WorkflowDbContext _dbContext;
+
+        public DocumentFileLocationFactory(WorkflowDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IDocumentFileLocationProcessor GetDocumentFileLocationProcessor(SourceType sourceType)
+        {
+            switch (sourceType)
+            {
+                case SourceType.Primary:
+                    return new PrimaryDocumentFileLocationProcessor(_dbContext);
+                case SourceType.Linked:
+                    return new LinkedDocumentFileLocationProcessor(_dbContext);
+                case SourceType.Database:
+                    return new DatabaseDocumentFileLocationProcessor(_dbContext);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sourceType), sourceType, null);
+            }
+        }
+    }
+}
--- src/Common.Factories/SourceDocumentHelper.cs
+++ src/Common.Factories/SourceDocumentHelper.cs
@@ -11,11 +11,18 @@ namespace Common.Factories
         public static async Task<int> UpdateSourceDocumentStatus(IDocumentStatusFactory documentStatusFactory,
             int processId, int sourceDocumentId,
             SourceDocumentRetrievalStatus status, SourceType sourceType, Guid? correlationId,
-            string sourceDocumentName = null, string sourceDocumentType = null,
-            string generatedFullFilename = null)
+            string sourceDocumentName = null, string sourceDocumentType = null)
         {
             var documentStatusProcessor = documentStatusFactory.GetDocumentStatusProcessor(sourceType);
-            return await documentStatusProcessor.Update(processId, sourceDocumentId, sourceDocumentName, sourceDocumentType, status, correlationId, generatedFullFilename);
+            return await documentStatusProcessor.Update(processId, sourceDocumentId, sourceDocumentName, sourceDocumentType, status, correlationId);
         }
+
+        public static async Task<int> UpdateSourceDocumentFileLocation(IDocumentFileLocationFactory documentFileLocationFactory,
+            int processId, int sourceDocumentId, SourceType sourceType, Guid contentServiceId, string generatedFullFilename)
+        {
+            var documentStatusProcessor = documentFileLocationFactory.GetDocumentFileLocationProcessor(sourceType);
+            return await documentStatusProcessor.Update(processId, sourceDocumentId, contentServiceId, generatedFullFilename);
+        }
+
     }
 }
