8215744cea6e52d305d6a30cd0e88e214496d045
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-16 14:15:17 +0100

    Updated SourceDocumentPartial to use ContentServiceUri property within PrimaryDocumentStatus EF, populate ContentServiceUri for DatabaseDocumentStatus

--- src/Portal/Portal/Pages/DbAssessment/_SourceDocumentDetails.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/_SourceDocumentDetails.cshtml.cs
@@ -35,7 +35,6 @@ namespace Portal.Pages.DbAssessment
         public IEnumerable<LinkedDocument> AttachedLinkedDocuments { get; set; }
         public PrimaryDocumentStatus PrimaryDocumentStatus { get; set; }
         public IEnumerable<DatabaseDocumentStatus> DatabaseDocuments { get; set; }
-        public Uri PrimaryDocumentContentServiceUri { get; set; }
 
         public _SourceDocumentDetailsModel(WorkflowDbContext dbContext,
             IOptions<UriConfig> uriConfig, IEventServiceApiClient eventServiceApiClient,
@@ -119,6 +118,14 @@ namespace Portal.Pages.DbAssessment
                 DatabaseDocuments = _dbContext
                     .DatabaseDocumentStatus
                     .Where(c => c.ProcessId == ProcessId).ToList();
+
+                foreach (var databaseDocumentStatus in DatabaseDocuments)
+                {
+                    if (databaseDocumentStatus.ContentServiceId.HasValue)
+                        databaseDocumentStatus.ContentServiceUri =
+                            _uriConfig.Value.BuildContentServiceUri(databaseDocumentStatus.ContentServiceId.Value);
+                }
+
             }
             catch (ArgumentNullException e)
             {
@@ -136,7 +143,7 @@ namespace Portal.Pages.DbAssessment
                 PrimaryDocumentStatus = _dbContext.PrimaryDocumentStatus.First(s => s.ProcessId == ProcessId);
 
                 if (PrimaryDocumentStatus.ContentServiceId.HasValue)
-                    PrimaryDocumentContentServiceUri =
+                    PrimaryDocumentStatus.ContentServiceUri =
                         _uriConfig.Value.BuildContentServiceUri(PrimaryDocumentStatus.ContentServiceId.Value);
             }
             catch (InvalidOperationException e)
