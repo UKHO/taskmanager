36d1369b7440144376124bd2a6598bd8391f73e8
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-29 17:37:47 +0100

    Added error dialog

--- src/Portal/Portal/Pages/DbAssessment/HistoricalTasks.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/HistoricalTasks.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Portal.Models;
 
@@ -10,13 +11,20 @@ namespace Portal.Pages.DbAssessment
         [BindProperty(SupportsGet = true)]
         public HistoricalTasksSearchParameters SearchParameters { get; set; }
 
+        public List<string> ErrorMessages { get; set; }
+
         public void OnGet()
         {
-
+            ErrorMessages = new List<string>();
         }
 
         public void OnPost()
         {
+            ErrorMessages = new List<string>()
+            {
+                "Error1",
+                "Error2"
+            };
 
         }
     }
