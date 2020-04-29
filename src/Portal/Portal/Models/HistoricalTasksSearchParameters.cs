0a7edc5ecf3fab8bf614dc0e91efeb457c6f3156
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-29 15:15:15 +0100

    Added functionality for the search box

---
+++ src/Portal/Portal/Models/HistoricalTasksSearchParameters.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Portal.Models
+{
+    public class HistoricalTasksSearchParameters
+    {
+        [DisplayName("Process Id:")]
+        public int ProcessId { get; set; }
+        [DisplayName("Sdoc Id:")]
+        public int SourceDocumentId { get; set; }
+        [DisplayName("RSDRA Number:")]
+        public string RsdraNumber { get; set; }
+        [DisplayName("Source Name:")]
+        public string SourceDocumentName { get; set; }
+        [DisplayName("Reviewer:")]
+        public string Reviewer { get; set; }
+        [DisplayName("Assessor:")]
+        public string Assessor { get; set; }
+        [DisplayName("Verifier:")]
+        public string Verifier { get; set; }
+    }
+}
--- src/Portal/Portal/Pages/DbAssessment/HistoricalTasks.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/HistoricalTasks.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portal.Models;
 
 namespace Portal.Pages.DbAssessment
 {
     public class HistoricalTasksModel : PageModel
     {
+
+        [BindProperty(SupportsGet = true)]
+        public HistoricalTasksSearchParameters SearchParameters { get; set; }   
         public void OnGet()
         {
 
