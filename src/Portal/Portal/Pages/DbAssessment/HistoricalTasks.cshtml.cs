d007f10b92dd5c10037fbf16c636abfd5109e017
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-04-29 15:33:46 +0100

    Added Search button to search form

--- src/Portal/Portal/Pages/DbAssessment/HistoricalTasks.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/HistoricalTasks.cshtml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Portal.Models;
 
@@ -12,7 +8,8 @@ namespace Portal.Pages.DbAssessment
     {
 
         [BindProperty(SupportsGet = true)]
-        public HistoricalTasksSearchParameters SearchParameters { get; set; }   
+        public HistoricalTasksSearchParameters SearchParameters { get; set; }
+
         public void OnGet()
         {
 
