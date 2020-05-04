668bfeea37960cf650ae4e2afccc6a8ac3a01a76
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-05-04 17:39:07 +0100

    Re-arranged Reviewer column position within DbAssessmentReviewData table

--- src/Databases/WorkflowDatabase/Tables/DbAssessmentReviewData.sql
+++ src/Databases/WorkflowDatabase/Tables/DbAssessmentReviewData.sql
@@ -6,11 +6,11 @@
     [ActivityCode] NVARCHAR(50) NULL, 
     [SourceCategory] NVARCHAR(255) NULL, 
     [WorkflowInstanceId] INT NOT NULL, 
+    [Reviewer] NVARCHAR(255) NULL, 
 	[Assessor] NVARCHAR(255) NULL, 
     [Verifier] NVARCHAR(255) NULL, 
 	[TaskType] NVARCHAR(50) NULL,
 	[WorkspaceAffected] NVARCHAR(100) NULL,
-    [Reviewer] NVARCHAR(255) NULL, 
 	[Notes] NVARCHAR(4000) NULL,
     CONSTRAINT [FK_DbAssessmentReviewData_WorkflowInstance] FOREIGN KEY ([WorkflowInstanceId]) REFERENCES [WorkflowInstance]([WorkflowInstanceId])
 )
