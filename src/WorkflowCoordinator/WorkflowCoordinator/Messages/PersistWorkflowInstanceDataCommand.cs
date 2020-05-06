f8ba421d3cfaa7e00519612107c4ff0f53eff117
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-05-06 16:17:34 +0100

    Added new command PersistWorkflowInstanceDataCommand that will eventually replace PersistWorkflowInstanceDataEvent

---
+++ src/WorkflowCoordinator/WorkflowCoordinator/Messages/PersistWorkflowInstanceDataCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Common.Messages;
+using WorkflowDatabase.EF;
+
+namespace WorkflowCoordinator.Messages
+{
+    public class PersistWorkflowInstanceDataCommand : ICorrelate
+    {
+        public Guid CorrelationId { get; set; }
+        public int ProcessId { get; set; }
+        public WorkflowStage FromActivity { get; set; }
+        public WorkflowStage ToActivity { get; set; }
+    }
+}
