a1769c087e8a52c82c1ac63e9fa16f6832b8c706
247benji <42581204+247benji@users.noreply.github.com>
2020-05-07 17:32:18 +0100

    Added test to ensure exception is thrown when task fails to progress in K2.
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>

--- src/WorkflowCoordinator/WorkflowCoordinator.UnitTests/ProgressWorkflowInstanceEventHandlerTests.cs
+++ src/WorkflowCoordinator/WorkflowCoordinator.UnitTests/ProgressWorkflowInstanceEventHandlerTests.cs
@@ -493,5 +493,69 @@ namespace WorkflowCoordinator.UnitTests
             Assert.IsNotNull(persistWorkflowInstanceDataCommand, $"No message of type {nameof(PersistWorkflowInstanceDataCommand)} seen.");
         }
 
+        [Test]
+        public async Task Test_Handle_ProgressWorkflowInstanceEvent_When_K2_Task_Returns_At_Assess_And_Task_Failed_To_Progress_In_K2_Then_ApplicationException_Is_Thrown()
+        {
+            var k2SerialNumber = "234_123";
+            var processId = 234;
+            var workflowInstanceId = 1;
+
+            var message = new ProgressWorkflowInstanceEvent()
+            {
+                CorrelationId = Guid.Empty,
+                ProcessId = processId,
+                FromActivity = WorkflowStage.Assess,
+                ToActivity = WorkflowStage.Verify
+            };
+
+            A.CallTo(() => _fakeWorkflowServiceApiClient.GetWorkflowInstanceData(message.ProcessId))
+                .Returns(new K2TaskData()
+                {
+                    ActivityName = WorkflowStage.Assess.ToString(),
+                    SerialNumber = k2SerialNumber,
+                    WorkflowInstanceID = processId
+                });
+
+            A.CallTo(() => _fakeWorkflowServiceApiClient.ProgressWorkflowInstance(processId, k2SerialNumber))
+                .Returns(false);
+
+            var currentWorkflowInstance = new WorkflowInstance()
+            {
+                ProcessId = processId,
+                WorkflowInstanceId = workflowInstanceId,
+                ActivityName = WorkflowStage.Assess.ToString(),
+                Status = WorkflowStatus.Updating.ToString(),
+                SerialNumber = k2SerialNumber
+            };
+            await _dbContext.WorkflowInstance.AddAsync(currentWorkflowInstance);
+            await _dbContext.SaveChangesAsync();
+
+            var assessData = new DbAssessmentAssessData()
+            {
+                WorkflowInstanceId = workflowInstanceId,
+                ProcessId = processId
+            };
+            await _dbContext.DbAssessmentAssessData.AddAsync(assessData);
+            await _dbContext.SaveChangesAsync();
+
+            //When
+
+            var ex = Assert.ThrowsAsync<ApplicationException>(() =>
+                _handler.Handle(message, _handlerContext));
+
+
+            //Then
+            A.CallTo(() => _fakeWorkflowServiceApiClient.ProgressWorkflowInstance(processId, k2SerialNumber))
+    .MustHaveHappened();
+
+            Assert.AreEqual($"Unable to progress task {message.ProcessId} from {message.FromActivity} to {message.ToActivity} in K2.", ex.Message);
+
+            Assert.AreEqual(0, _handlerContext.SentMessages.Length);
+
+            var persistWorkflowInstanceDataCommand = _handlerContext.SentMessages.SingleOrDefault(t =>
+                t.Message is PersistWorkflowInstanceDataCommand);
+            Assert.IsNull(persistWorkflowInstanceDataCommand, $"Message of type {nameof(PersistWorkflowInstanceDataCommand)} seen.");
+        }
+
     }
 }
