f52b1d0e69f9a6b02bc000930199b46a7096b7ac
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-05-07 10:49:04 +0100

    Code tidy

--- src/WorkflowCoordinator/WorkflowCoordinator.UnitTests/PersistWorkflowInstanceDataEventHandlerTests.cs
+++ src/WorkflowCoordinator/WorkflowCoordinator.UnitTests/PersistWorkflowInstanceDataEventHandlerTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Common.Messages.Events;
 using FakeItEasy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
