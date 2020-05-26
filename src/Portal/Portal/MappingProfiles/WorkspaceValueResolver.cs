55085488934da4e3c1e5d2aeb0bb64b652a2c552
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2020-05-26 16:48:31 +0100

    Add WorkspaceValueResolver to map Workspace

---
+++ src/Portal/Portal/MappingProfiles/WorkspaceValueResolver.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Portal.ViewModels;
+using WorkflowDatabase.EF.Models;
+
+namespace Portal.MappingProfiles
+{
+    public class WorkspaceValueResolver : IValueResolver<WorkflowInstance, TaskViewModel, string>
+    {
+        public string Resolve(WorkflowInstance source, TaskViewModel destination, string destMember, ResolutionContext context)
+        {
+            switch (source.ActivityName)
+            {
+                case "Review":
+                    return source.DbAssessmentReviewData != null ? source.DbAssessmentReviewData.WorkspaceAffected : string.Empty;
+                case "Assess":
+                    return source.DbAssessmentAssessData != null ? source.DbAssessmentAssessData.WorkspaceAffected : string.Empty;
+                case "Verify":
+                    return source.DbAssessmentVerifyData != null ? source.DbAssessmentVerifyData.WorkspaceAffected : string.Empty;
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
