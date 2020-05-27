5a3d36db04c11bd6cb63ad767a989830925665d9
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-05-27 19:33:03 +0100

    Converted StartWorkflowInstanceEvent into StartChildWorkflowInstanceCommand in workflowCoordinator

---
+++ src/WorkflowCoordinator/WorkflowCoordinator/Messages/StartChildWorkflowInstanceCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Common.Messages.Enums;
+
+namespace Common.Messages.Events
+{
+    public class StartChildWorkflowInstanceCommand : ICorrelate
+    {
+        public Guid CorrelationId { get; set; }
+        public WorkflowType WorkflowType { get; set; }
+        public int ParentProcessId { get; set; }
+        public int AssignedTaskId { get; set; }
+    }
+}
+    
