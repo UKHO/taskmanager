55c5f0d9090b7d47c52f78c61934b9c72d165f38
Rajan <23729848+ssrajan@users.noreply.github.com>
2020-06-01 09:08:37 +0100

    NCNE : Enable publish button only valid chart numbers (#473)
    * NCNE : Enable publish button only valid chart numbers

--- src/NCNEPortal.UnitTests/PageValidationTests.cs
+++ src/NCNEPortal.UnitTests/PageValidationTests.cs
@@ -1,5 +1,4 @@
-﻿using Common.Helpers.Auth;
-using FakeItEasy;
+﻿using FakeItEasy;
 using Microsoft.EntityFrameworkCore;
 using NCNEPortal.Auth;
 using NCNEPortal.Enums;
@@ -15,8 +14,6 @@ namespace NCNEPortal.UnitTests
     [TestFixture]
     public class PageValidationTests
     {
-        private NcneWorkflowDbContext _dbContext;
-        private IAdDirectoryService _fakeAdDirectoryService;
         private INcneUserDbService _fakeNcneUserDbService;
         private PageValidationHelper _pageValidationHelper;
         private List<AdUser> _validUsers;
@@ -29,9 +26,6 @@ namespace NCNEPortal.UnitTests
                 .UseInMemoryDatabase(databaseName: "inmemory")
                 .Options;
 
-            _dbContext = new NcneWorkflowDbContext(dbContextOptions);
-
-            _fakeAdDirectoryService = A.Fake<IAdDirectoryService>();
             _fakeNcneUserDbService = A.Fake<INcneUserDbService>();
 
             _pageValidationHelper = new PageValidationHelper(_fakeNcneUserDbService);
