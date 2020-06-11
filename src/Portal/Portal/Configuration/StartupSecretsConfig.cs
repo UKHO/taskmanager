5d0bb59a8efe59de6c5a3ad7ab3ca6708900f5ae
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-06-11 15:38:36 +0100

    Removed unused properties from Portal config

--- src/Portal/Portal/Configuration/StartupSecretsConfig.cs
+++ src/Portal/Portal/Configuration/StartupSecretsConfig.cs
@@ -5,8 +5,6 @@
         public string DataSource { get; set; }
         public string UserId { get; set; }
         public string Password { get; set; }
-        public string K2RestApiUsername { get; set; }
-        public string K2RestApiPassword { get; set; }
         public string SqlLoggingUsername { get; set; }
         public string SqlLoggingPassword { get; set; }
         public string ClientAzureAdSecret { get; set; }
