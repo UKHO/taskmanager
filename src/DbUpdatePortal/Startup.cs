732308d5b42022630392d740fa5d6438ece1fbec
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-06-19 09:40:26 +0100

    Add app versioning stuff ready for pipeline to DbUpdate

--- src/DbUpdatePortal/Startup.cs
+++ src/DbUpdatePortal/Startup.cs
@@ -1,3 +1,4 @@
+using DbUpdatePortal.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +20,8 @@ namespace DbUpdatePortal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddSingleton<AppVersionInfo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
