ea0aa8182ae9cdaa3014dcc18307a1473a3bc06b
Rajan Shunmuga <23729848+ssrajan@users.noreply.github.com>
2020-06-30 10:40:11 +0100

    NCNE : renaming 100PercentCheck column name to HundredPercentCheck

--- src/Databases/NCNEWorkflowDatabase.EF/Models/TaskRole.cs
+++ src/Databases/NCNEWorkflowDatabase.EF/Models/TaskRole.cs
@@ -12,7 +12,6 @@ namespace NCNEWorkflowDatabase.EF.Models
         public string Compiler { get; set; }
         public string VerifierOne { get; set; }
         public string VerifierTwo { get; set; }
-        [Column("100PercentCheck")]
         public string HundredPercentCheck { get; set; }
     }
 }
