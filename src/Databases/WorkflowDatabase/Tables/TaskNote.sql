f3101c9382f84b35897c005939804e07b3585fb1
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-02 08:50:51 +0100

    DbAssessment AdUsers FK linking: database changes.

--- src/Databases/WorkflowDatabase/Tables/TaskNote.sql
+++ src/Databases/WorkflowDatabase/Tables/TaskNote.sql
@@ -4,9 +4,12 @@
     [ProcessId] INT NOT NULL, 
     [Text] NVARCHAR(MAX) NOT NULL, 
     [WorkflowInstanceId] INT NOT NULL, 
-    [CreatedByUsername] NVARCHAR(255) NOT NULL, 
+    [CreatedByAdUserId] INT NOT NULL, 
     [Created] DATETIME NOT NULL, 
     [LastModified] DATETIME NOT NULL, 
-    [LastModifiedByUsername] NVARCHAR(255) NOT NULL, 
-    CONSTRAINT [FK_TaskNote_WorkflowInstance] FOREIGN KEY ([WorkflowInstanceId]) REFERENCES [WorkflowInstance]([WorkflowInstanceId])
+    [LastModifiedByAdUserId] INT NOT NULL, 
+    CONSTRAINT [FK_TaskNote_WorkflowInstance] FOREIGN KEY ([WorkflowInstanceId]) REFERENCES [WorkflowInstance]([WorkflowInstanceId]),   
+    CONSTRAINT [FK_TaskNote_CreatedByAdUserId] FOREIGN KEY ([CreatedByAdUserId]) REFERENCES [AdUsers]([AdUserId]),
+    CONSTRAINT [FK_TaskNote_LastModifiedByUserId] FOREIGN KEY ([LastModifiedByAdUserId]) REFERENCES [AdUsers]([AdUserId])
+
 )
