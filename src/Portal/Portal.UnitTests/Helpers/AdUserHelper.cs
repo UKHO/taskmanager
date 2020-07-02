0259f45454dc516af4fcac19dd2b2b3bfa78efda
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-02 16:43:52 +0100

    Create AdUser helper for tests.

---
+++ src/Portal/Portal.UnitTests/Helpers/AdUserHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using WorkflowDatabase.EF;
+using WorkflowDatabase.EF.Models;
+
+namespace Portal.UnitTests.Helpers
+{
+    internal static class AdUserHelper
+    {
+        internal static AdUser CreateTestUser(WorkflowDbContext dbContext, int uniqueNumber = 0)
+        {
+            var email = "test@email.com";
+            var name = "Test User";
+
+            if (uniqueNumber > 0)
+            {
+                email = $"test{uniqueNumber}@email.com";
+                name = $"Test User {uniqueNumber}";
+            }
+
+
+            var user = dbContext.AdUsers.SingleOrDefault(u =>
+                u.UserPrincipalName.Equals(email, StringComparison.OrdinalIgnoreCase));
+            if (user != null) return user;
+
+            user = new AdUser
+            {
+                DisplayName = name,
+                UserPrincipalName = email
+            };
+            dbContext.AdUsers.Add(user);
+            dbContext.SaveChanges();
+
+            return user;
+        }
+    }
+}
