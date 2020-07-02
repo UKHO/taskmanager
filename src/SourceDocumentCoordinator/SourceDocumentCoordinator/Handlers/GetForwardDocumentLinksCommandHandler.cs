585ab13bfa1ed7c67d5927704f8ea7cc8c9a63d3
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-07-02 12:04:14 +0100

    Add Linked documents to DB even when no assessment data was found

--- src/SourceDocumentCoordinator/SourceDocumentCoordinator/Handlers/GetForwardDocumentLinksCommandHandler.cs
+++ src/SourceDocumentCoordinator/SourceDocumentCoordinator/Handlers/GetForwardDocumentLinksCommandHandler.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common.Helpers;
 using Common.Messages.Commands;
+using DataServices.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 using Serilog.Context;
 using SourceDocumentCoordinator.HttpClients;
 using WorkflowDatabase.EF;
-using WorkflowDatabase.EF.Models;
+using LinkedDocument = WorkflowDatabase.EF.Models.LinkedDocument;
 
 namespace SourceDocumentCoordinator.Handlers
 {
@@ -55,78 +55,105 @@ namespace SourceDocumentCoordinator.Handlers
 
             LogContext.PushProperty("LinkedDocuments", linkedDocIds.ToJSONSerializedString());
 
-            _logger.LogInformation("Forward linked documents found for SourceDocumentId: {SourceDocumentId}; LinkedDocuments: {LinkedDocuments}");
+            _logger.LogInformation("Forward linked documents found for Source Document: {SourceDocumentId}; Linked Documents: {LinkedDocuments}");
 
             var failedLinkedDocuments = new StringBuilder(linkedDocIds.Count);
 
-            await PersistLinkedDocuments(message, linkedDocIds, failedLinkedDocuments);
+            foreach (var linkedDocId in linkedDocIds)
+            {
+
+                LogContext.PushProperty("LinkedDocument", linkedDocId);
+
+                _logger.LogInformation(
+                    "Processing forward linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+
+
+                var linkedDocumentAssessmentData =
+                    await GetLinkedDocumentAssessmentData(linkedDocId, failedLinkedDocuments);
+
+                await PersistLinkedDocuments(message, linkedDocId, linkedDocumentAssessmentData, failedLinkedDocuments);
+            }
+            
+            await _dbContext.SaveChangesAsync();
 
             if (failedLinkedDocuments.Length > 0)
             {
                 throw new ApplicationException(
-                    $"Errors while adding Backward linked documents to SourceDocumentId: {message.SourceDocumentId}:" +
+                    $"Errors while adding Forward linked documents to SourceDocumentId: {message.SourceDocumentId}:" +
                     $"{Environment.NewLine}{failedLinkedDocuments}");
             }
 
             _logger.LogInformation("Successfully Completed Event {EventName}: {Message}");
         }
-
-        private async Task PersistLinkedDocuments(GetForwardDocumentLinksCommand message, IEnumerable<int> linkedDocIds,
-            StringBuilder failedLinkedDocuments)
+        
+        private async Task<DocumentAssessmentData> GetLinkedDocumentAssessmentData(int linkedDocId, StringBuilder failedLinkedDocuments)
         {
-            foreach (var linkedDocId in linkedDocIds)
+            DocumentAssessmentData documentAssessmentData = null;
+            try
             {
-                LogContext.PushProperty("LinkedDocument", linkedDocId);
+                documentAssessmentData = await _dataServiceApiClient.GetAssessmentData(linkedDocId);
 
                 _logger.LogInformation(
-                    "Adding Forward LinkedDocument: {LinkedDocument} to SourceDocumentId: {SourceDocumentId}");
+                    "Successfully retrieved assessment data for forward linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get assessment data for forward linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
 
-                try
+                failedLinkedDocuments.AppendLine(
+                    $"Failed to get assessment data for forward linked document: {linkedDocId}: {e.Message}");
+            }
+
+            return documentAssessmentData;
+        }
+
+        private async Task PersistLinkedDocuments(GetForwardDocumentLinksCommand message, int linkedDocId, DocumentAssessmentData documentAssessmentData, StringBuilder failedLinkedDocuments)
+        {
+            try
+            {
+                var linkedDocument = await _dbContext.LinkedDocument.FirstOrDefaultAsync(l =>
+                                                                            l.ProcessId == message.ProcessId
+                                                                            && l.LinkedSdocId == documentAssessmentData.SdocId
+                                                                            && l.LinkType == DocumentLinkType.Forward.ToString());
+
+                var isNew = linkedDocument == null;
+
+                if (isNew)
                 {
-                    var documentAssessmentData = await _dataServiceApiClient.GetAssessmentData(linkedDocId);
+                    linkedDocument = new LinkedDocument();
+                }
 
-                    var linkedDocument = await _dbContext.LinkedDocument.FirstOrDefaultAsync(l =>
-                        l.ProcessId == message.ProcessId
-                        && l.LinkedSdocId == documentAssessmentData.SdocId
-                        && l.LinkType == DocumentLinkType.Forward.ToString());
+                linkedDocument.ProcessId = message.ProcessId;
+                linkedDocument.PrimarySdocId = message.SourceDocumentId;
+                linkedDocument.LinkedSdocId = linkedDocId;
+                linkedDocument.LinkType = DocumentLinkType.Forward.ToString();
+                linkedDocument.Status = SourceDocumentRetrievalStatus.NotAttached.ToString();
+                linkedDocument.Created = DateTime.Now;
 
-                    var isNew = linkedDocument == null;
-
-                    if (isNew)
-                    {
-                        linkedDocument = new LinkedDocument();
-                    }
-
-                    linkedDocument.ProcessId = message.ProcessId;
-                    linkedDocument.PrimarySdocId = message.SourceDocumentId;
-                    linkedDocument.LinkedSdocId = documentAssessmentData.SdocId;
+                if (documentAssessmentData != null)
+                {
                     linkedDocument.RsdraNumber = documentAssessmentData.SourceName;
                     linkedDocument.SourceDocumentName = documentAssessmentData.Name;
                     linkedDocument.ReceiptDate = documentAssessmentData.ReceiptDate;
                     linkedDocument.SourceDocumentType = documentAssessmentData.DocumentType;
                     linkedDocument.SourceNature = documentAssessmentData.SourceName;
                     linkedDocument.Datum = documentAssessmentData.Datum;
-                    linkedDocument.LinkType = DocumentLinkType.Forward.ToString();
-                    linkedDocument.Status = SourceDocumentRetrievalStatus.NotAttached.ToString();
-                    linkedDocument.Created = DateTime.Now;
-
-                    if (isNew)
-                    {
-                        await _dbContext.LinkedDocument.AddAsync(linkedDocument);
-                    }
-
-                    await _dbContext.SaveChangesAsync();
-
-                    _logger.LogInformation(
-                        "Successfully added Forward LinkedDocument: {LinkedDocument} to SourceDocumentId: {SourceDocumentId}");
                 }
-                catch (Exception e)
+
+                if (isNew)
                 {
-                    _logger.LogError(e, "Failed to add Forward LinkedDocument: {LinkedDocument} to SourceDocumentId: {SourceDocumentId}");
-
-                    failedLinkedDocuments.AppendLine(
-                        $"Failed to add Forward LinkedDocument: {linkedDocId} to SourceDocumentId: {message.SourceDocumentId}: {e.Message}");
+                    await _dbContext.LinkedDocument.AddAsync(linkedDocument);
                 }
+
+                _logger.LogInformation(
+                    "Successfully added forward linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to add forward linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+
+                failedLinkedDocuments.AppendLine(
+                    $"Failed to add forward linked document: {linkedDocId}: {e.Message}");
             }
         }
     }
--- src/SourceDocumentCoordinator/SourceDocumentCoordinator/Handlers/GetSepDocumentLinksCommandHandler.cs
+++ src/SourceDocumentCoordinator/SourceDocumentCoordinator/Handlers/GetSepDocumentLinksCommandHandler.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common.Helpers;
 using Common.Messages.Commands;
+using DataServices.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 using Serilog.Context;
 using SourceDocumentCoordinator.HttpClients;
 using WorkflowDatabase.EF;
-using WorkflowDatabase.EF.Models;
+using LinkedDocument = WorkflowDatabase.EF.Models.LinkedDocument;
 
 namespace SourceDocumentCoordinator.Handlers
 {
@@ -59,74 +59,101 @@ namespace SourceDocumentCoordinator.Handlers
 
             var failedLinkedDocuments = new StringBuilder(linkedDocIds.Count);
 
-            await PersistLinkedDocuments(message, linkedDocIds, failedLinkedDocuments);
+            foreach (var linkedDocId in linkedDocIds)
+            {
+
+                LogContext.PushProperty("LinkedDocument", linkedDocId);
+
+                _logger.LogInformation(
+                    "Processing sep linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+
+
+                var linkedDocumentAssessmentData =
+                    await GetLinkedDocumentAssessmentData(linkedDocId, failedLinkedDocuments);
+
+                await PersistLinkedDocuments(message, linkedDocId, linkedDocumentAssessmentData, failedLinkedDocuments);
+            }
+
+            await _dbContext.SaveChangesAsync();
 
             if (failedLinkedDocuments.Length > 0)
             {
                 throw new ApplicationException(
-                    $"Errors while adding Backward linked documents to SourceDocumentId: {message.SourceDocumentId}:" +
+                    $"Errors while adding Sep linked documents to SourceDocumentId: {message.SourceDocumentId}:" +
                     $"{Environment.NewLine}{failedLinkedDocuments}");
             }
 
             _logger.LogInformation("Successfully Completed Event {EventName}: {Message}");
         }
 
-        private async Task PersistLinkedDocuments(GetSepDocumentLinksCommand message, IEnumerable<int> linkedDocIds,
-            StringBuilder failedLinkedDocuments)
+        private async Task<DocumentAssessmentData> GetLinkedDocumentAssessmentData(int linkedDocId, StringBuilder failedLinkedDocuments)
         {
-            foreach (var linkedDocId in linkedDocIds)
+            DocumentAssessmentData documentAssessmentData = null;
+            try
             {
-                LogContext.PushProperty("LinkedDocument", linkedDocId);
+                documentAssessmentData = await _dataServiceApiClient.GetAssessmentData(linkedDocId);
 
                 _logger.LogInformation(
-                    "Adding SEP LinkedDocument: {LinkedDocument} to SourceDocumentId: {SourceDocumentId}");
+                    "Successfully retrieved assessment data for sep linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get assessment data for sep linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
 
-                try
+                failedLinkedDocuments.AppendLine(
+                    $"Failed to get assessment data for sep linked document: {linkedDocId}: {e.Message}");
+            }
+
+            return documentAssessmentData;
+        }
+
+        private async Task PersistLinkedDocuments(GetSepDocumentLinksCommand message, int linkedDocId, DocumentAssessmentData documentAssessmentData, StringBuilder failedLinkedDocuments)
+        {
+            try
+            {
+                var linkedDocument = await _dbContext.LinkedDocument.FirstOrDefaultAsync(l =>
+                                                                            l.ProcessId == message.ProcessId
+                                                                            && l.LinkedSdocId == documentAssessmentData.SdocId
+                                                                            && l.LinkType == DocumentLinkType.Sep.ToString());
+
+                var isNew = linkedDocument == null;
+
+                if (isNew)
                 {
-                    var documentAssessmentData = await _dataServiceApiClient.GetAssessmentData(linkedDocId);
+                    linkedDocument = new LinkedDocument();
+                }
 
-                    var linkedDocument = await _dbContext.LinkedDocument.FirstOrDefaultAsync(l =>
-                        l.ProcessId == message.ProcessId
-                        && l.LinkedSdocId == documentAssessmentData.SdocId
-                        && l.LinkType == DocumentLinkType.Sep.ToString());
+                linkedDocument.ProcessId = message.ProcessId;
+                linkedDocument.PrimarySdocId = message.SourceDocumentId;
+                linkedDocument.LinkedSdocId = linkedDocId;
+                linkedDocument.LinkType = DocumentLinkType.Sep.ToString();
+                linkedDocument.Status = SourceDocumentRetrievalStatus.NotAttached.ToString();
+                linkedDocument.Created = DateTime.Now;
 
-                    var isNew = linkedDocument == null;
-
-                    if (isNew)
-                    {
-                        linkedDocument = new LinkedDocument();
-                    }
-
-                    linkedDocument.ProcessId = message.ProcessId;
-                    linkedDocument.PrimarySdocId = message.SourceDocumentId;
-                    linkedDocument.LinkedSdocId = documentAssessmentData.SdocId;
+                if (documentAssessmentData != null)
+                {
                     linkedDocument.RsdraNumber = documentAssessmentData.SourceName;
                     linkedDocument.SourceDocumentName = documentAssessmentData.Name;
                     linkedDocument.ReceiptDate = documentAssessmentData.ReceiptDate;
                     linkedDocument.SourceDocumentType = documentAssessmentData.DocumentType;
                     linkedDocument.SourceNature = documentAssessmentData.SourceName;
                     linkedDocument.Datum = documentAssessmentData.Datum;
-                    linkedDocument.LinkType = DocumentLinkType.Sep.ToString();
-                    linkedDocument.Status = SourceDocumentRetrievalStatus.NotAttached.ToString();
-                    linkedDocument.Created = DateTime.Now;
-
-                    if (isNew)
-                    {
-                        await _dbContext.LinkedDocument.AddAsync(linkedDocument);
-                    }
-
-                    await _dbContext.SaveChangesAsync();
-
-                    _logger.LogInformation(
-                        "Successfully added SEP LinkedDocument: {LinkedDocument} to SourceDocumentId: {SourceDocumentId}");
                 }
-                catch (Exception e)
+
+                if (isNew)
                 {
-                    _logger.LogError(e, "Failed to add SEP LinkedDocument: {LinkedDocument} to SourceDocumentId: {SourceDocumentId}");
-
-                    failedLinkedDocuments.AppendLine(
-                        $"Failed to add SEP LinkedDocument: {linkedDocId} to SourceDocumentId: {message.SourceDocumentId}: {e.Message}");
+                    await _dbContext.LinkedDocument.AddAsync(linkedDocument);
                 }
+
+                _logger.LogInformation(
+                    "Successfully added sep linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to add sep linked document: {LinkedDocument} to Source Document: {SourceDocumentId}");
+
+                failedLinkedDocuments.AppendLine(
+                    $"Failed to add sep linked document: {linkedDocId}: {e.Message}");
             }
         }
     }
