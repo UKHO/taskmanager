7038153e8453f5d25816de096fffcd2f219af1bf
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-02 09:02:57 +0100

    DbAssessment AdUsers FK linking: WIP seeding.

--- src/Common.Helpers/TestWorkflowDatabaseSeeder.cs
+++ src/Common.Helpers/TestWorkflowDatabaseSeeder.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using WorkflowDatabase.EF;
 using WorkflowDatabase.EF.Models;
@@ -9,22 +12,24 @@ namespace Common.Helpers
 {
     public class TestWorkflowDatabaseSeeder : ICanPopulateTables, ICanSaveChanges
     {
-        protected readonly WorkflowDbContext _context;
+        private readonly IServiceProvider _sp;
 
-        protected TestWorkflowDatabaseSeeder(WorkflowDbContext context)
+        protected TestWorkflowDatabaseSeeder(IServiceProvider sp)
         {
-            _context = context;
+            _sp = sp;
+
         }
 
-        public static ICanPopulateTables UsingDbContext(WorkflowDbContext context)
+        public static ICanPopulateTables UsingDbContext(IServiceProvider sp)
         {
-            return new TestWorkflowDatabaseSeeder(context);
+            return new TestWorkflowDatabaseSeeder(sp);
         }
 
         public ICanSaveChanges PopulateTables()
         {
-
-            DatabasesHelpers.ClearWorkflowDbTables(_context);
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
+            DatabasesHelpers.ClearWorkflowDbTables(workflowDbContext);
 
             AddAdditionalAdUsers();
 
@@ -43,8 +48,12 @@ namespace Common.Helpers
 
             var jsonString = File.ReadAllText(@"Data\Users.json");
             var users = JsonConvert.DeserializeObject<IEnumerable<AdUser>>(jsonString);
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
 
-            if (users?.Any() ?? false) _context.AdUser.AddRange(users);
+            if (users?.Any() ?? false) workflowDbContext.AdUsers.AddRange(users);
+
+            workflowDbContext.SaveChanges();
         }
 
         private void PopulateHpdUsage()
@@ -54,7 +63,10 @@ namespace Common.Helpers
             var jsonString = File.ReadAllText(@"Data\HpdUsages.json");
             var hpdUsages = JsonConvert.DeserializeObject<IEnumerable<HpdUsage>>(jsonString);
 
-            _context.HpdUsage.AddRange(hpdUsages);
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
+            workflowDbContext.HpdUsage.AddRange(hpdUsages);
+            workflowDbContext.SaveChanges();
         }
 
         private void PopulateHpdUser()
@@ -63,8 +75,16 @@ namespace Common.Helpers
 
             var jsonString = File.ReadAllText(@"Data\HpdUsers.json");
             var hpdUsers = JsonConvert.DeserializeObject<IEnumerable<HpdUser>>(jsonString);
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
 
-            _context.HpdUser.AddRange(hpdUsers);
+            foreach (var hpdUser in hpdUsers)
+            {
+                workflowDbContext.Entry(hpdUser.AdUser).State = EntityState.Unchanged;
+                workflowDbContext.HpdUser.Add(hpdUser);
+            }
+
+            workflowDbContext.SaveChanges();
         }
 
         private void PopulateProductActionType()
@@ -73,8 +93,10 @@ namespace Common.Helpers
 
             var jsonString = File.ReadAllText(@"Data\ProductActionType.json");
             var productActionType = JsonConvert.DeserializeObject<IEnumerable<ProductActionType>>(jsonString);
-
-            _context.ProductActionType.AddRange(productActionType);
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
+            workflowDbContext.ProductActionType.AddRange(productActionType);
+            workflowDbContext.SaveChanges();
         }
 
         private void PopulateAssignedTaskSourceType()
@@ -83,9 +105,12 @@ namespace Common.Helpers
 
             var jsonString = File.ReadAllText(@"Data\AssignedTaskType.json");
             var assignedTaskSourceType = JsonConvert.DeserializeObject<IEnumerable<AssignedTaskType>>(jsonString);
-
-            _context.AssignedTaskType.AddRange(assignedTaskSourceType);
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
+            workflowDbContext.AssignedTaskType.AddRange(assignedTaskSourceType);
+            workflowDbContext.SaveChanges();
         }
+
 
         private void PopulateWorkflowInstance()
         {
@@ -94,12 +119,56 @@ namespace Common.Helpers
             var jsonString = File.ReadAllText(@"Data\TasksSeedData.json");
             var tasks = JsonConvert.DeserializeObject<IEnumerable<WorkflowInstance>>(jsonString);
 
-            _context.WorkflowInstance.AddRange(tasks);
+
+            //foreach (var item in tasks)
+            //{
+            //    _context.WorkflowInstance.Add(item);
+            //    _context.SaveChanges();
+            //}
+
+            //  var wi = _context.WorkflowInstance.Include(w => w.).ToList();
+
+            using var scope = _sp.CreateScope();
+            var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
+            var wi = workflowDbContext.WorkflowInstance
+                //.Include(w => w.Comments).ThenInclude(c => c.AdUser).ThenInclude(u => u.AdUserId).AsNoTracking()
+                //.Include(w => w.DbAssessmentAssessData).ThenInclude(a => a.Reviewer).ThenInclude(u => u.AdUserId).AsNoTracking()
+                //.Include(w => w.DbAssessmentReviewData).ThenInclude(a => a.Reviewer).ThenInclude(u => u.AdUserId).AsNoTracking()
+                //.Include(w => w.DbAssessmentAssessData).ThenInclude(a => a.Assessor).ThenInclude(u => u.AdUserId).AsNoTracking()
+                //.Include(w => w.DbAssessmentReviewData).ThenInclude(a => a.Assessor).ThenInclude(u => u.AdUserId).AsNoTracking()
+                //.Include(w => w.DbAssessmentAssessData).ThenInclude(a => a.Verifier).ThenInclude(u => u.AdUserId).AsNoTracking()
+                //.Include(w => w.DbAssessmentReviewData).ThenInclude(a => a.Verifier).ThenInclude(u => u.AdUserId).AsNoTracking()
+                .ToList();
+
+
+            foreach (var task in tasks)
+            {
+                foreach (var comment in task.Comments)
+                {
+                    workflowDbContext.Entry(comment.AdUser).State = EntityState.Unchanged;
+                }
+
+                if (task.DbAssessmentReviewData?.Assessor != null) workflowDbContext.Entry(task.DbAssessmentReviewData.Assessor).State = EntityState.Unchanged;
+                if (task.DbAssessmentReviewData?.Reviewer != null) workflowDbContext.Entry(task.DbAssessmentReviewData.Reviewer).State = EntityState.Unchanged;
+                if (task.DbAssessmentReviewData?.Verifier != null) workflowDbContext.Entry(task.DbAssessmentReviewData.Verifier).State = EntityState.Unchanged;
+                if (task.DbAssessmentVerifyData?.Assessor != null) workflowDbContext.Entry(task.DbAssessmentVerifyData.Assessor).State = EntityState.Unchanged;
+                if (task.DbAssessmentVerifyData?.Reviewer != null) workflowDbContext.Entry(task.DbAssessmentVerifyData.Reviewer).State = EntityState.Unchanged;
+                if (task.DbAssessmentVerifyData?.Verifier != null) workflowDbContext.Entry(task.DbAssessmentVerifyData.Verifier).State = EntityState.Unchanged;
+                if (task.DbAssessmentAssessData?.Assessor != null) workflowDbContext.Entry(task.DbAssessmentAssessData.Assessor).State = EntityState.Unchanged;
+                if (task.DbAssessmentAssessData?.Reviewer != null) workflowDbContext.Entry(task.DbAssessmentAssessData.Reviewer).State = EntityState.Unchanged;
+                if (task.DbAssessmentAssessData?.Verifier != null) workflowDbContext.Entry(task.DbAssessmentAssessData.Verifier).State = EntityState.Unchanged;
+
+                workflowDbContext.WorkflowInstance.Add(task);
+                workflowDbContext.SaveChanges();
+            }
+
+            //   wi.AddRange(tasks);
+            workflowDbContext.SaveChanges();
         }
 
         public void SaveChanges()
         {
-            _context.SaveChanges();
+            //_context.SaveChanges();
         }
     }
 
