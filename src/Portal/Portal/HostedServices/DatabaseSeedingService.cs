792accd1374cbe0c69853db34c7f313427ed78b6
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-02 08:55:59 +0100

    DbAssessment AdUsers FK linking: seed as hosted service.

---
+++ src/Portal/Portal/HostedServices/DatabaseSeedingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Helpers;
+using Microsoft.Extensions.Hosting;
+
+namespace Portal.HostedServices
+{
+    public class DatabaseSeedingService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DatabaseSeedingService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            //using var scope = _serviceProvider.CreateScope();
+            //var workflowDbContext = scope.ServiceProvider.GetRequiredService<WorkflowDbContext>();
+
+            TestWorkflowDatabaseSeeder.UsingDbContext(_serviceProvider).PopulateTables().SaveChanges();
+
+            return Task.CompletedTask;
+        }
+
+        // Not required
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
