28085db3f12cd2d5460aa3f3f5acb50aeaccdad8
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2020-07-07 15:39:01 +0100

    Retrieve and display STS Data Impact

--- src/Portal/Portal/Pages/DbAssessment/_DataImpact.cshtml.cs
+++ src/Portal/Portal/Pages/DbAssessment/_DataImpact.cshtml.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
 using WorkflowDatabase.EF;
 using WorkflowDatabase.EF.Models;
 
@@ -13,25 +16,34 @@ namespace Portal.Pages.DbAssessment
     public class _DataImpactModel : PageModel
     {
         private readonly WorkflowDbContext _dbContext;
+        private readonly ILogger<_DataImpactModel> _logger;
 
         [BindProperty(SupportsGet = true)]
         public int ProcessId { get; set; }
 
         public List<DataImpact> DataImpacts { get; set; }
 
+        public DataImpact StsDataImpact { get; set; }
+
         public SelectList Usages { get; set; }
 
-        public _DataImpactModel(WorkflowDbContext dbContext)
+        public _DataImpactModel(WorkflowDbContext dbContext,
+            ILogger<_DataImpactModel> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         public async Task OnGetAsync(int processId)
         {
+            LogContext.PushProperty("ProcessId", processId);
+            LogContext.PushProperty("PortalResource", nameof(OnGetAsync));
+
             ProcessId = processId;
 
             await PopulateUsages();
             await SetDataImpactFromDb();
+            await SetStsDataImpactFromDb();
         }
 
         private async Task PopulateUsages()
@@ -52,6 +64,22 @@ namespace Portal.Pages.DbAssessment
                     new DataImpact()
                 };
             }
+        }
+
+        private async Task SetStsDataImpactFromDb()
+        {
+            try
+            {
+                StsDataImpact = await _dbContext.DataImpact
+                    .SingleOrDefaultAsync(di => di.ProcessId == ProcessId && di.StsUsage);
+            }
+            catch (InvalidOperationException exception)
+            {
+                _logger.LogError(exception,"Multiple STS Data Impact records found when only one is expected");
+                throw;
+            }
+
+            StsDataImpact ??= new DataImpact();
         }
     }
 }
