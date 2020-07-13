c0cfd1bf3c17e7b92e3e5fb6e9b4844026a087c5
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-07-13 14:07:51 +0100

    Add unit test project

---
+++ src/SourceDocumentService.UnitTests/SourceDocumentControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SourceDocumentService.UnitTests
+{
+    [TestFixture]
+    public class SourceDocumentControllerTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
