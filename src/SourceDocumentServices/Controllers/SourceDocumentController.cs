e95dde79bdb745219a362f5c071fcb882e29137e
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-07-13 13:32:46 +0100

    Add initial skeleton API

---
+++ src/SourceDocumentServices/Controllers/SourceDocumentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SourceDocumentService.Controllers
+{
+    [ApiController]
+    public class SourceDocumentController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        [Route("/SourceDocumentService/v1/")]
+        public string Get(int id)
+        {
+            return "Ok";
+        }
+
+        [Route("/SourceDocumentService/v1/")]
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
