0bfd56d9652eae6e9e5b4fbd088e267f98629776
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-07-13 14:42:17 +0100

    Implement starter Serilog text logging, and add the txt logs to gitignore

--- src/SourceDocumentService/Controllers/SourceDocumentController.cs
+++ src/SourceDocumentService/Controllers/SourceDocumentController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace SourceDocumentService.Controllers
 {
     [ApiController]
     public class SourceDocumentController : ControllerBase
     {
+        readonly ILogger<SourceDocumentController> _logger;
+
+        public SourceDocumentController(ILogger<SourceDocumentController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("{id}")]
         [Route("/SourceDocumentService/v1/")]
-        public string Get(int id)
+        public string Get()
         {
+            _logger.LogInformation("GET accessed at " + DateTime.Now);
             return "Ok";
         }
 
--- src/SourceDocumentService/Program.cs
+++ src/SourceDocumentService/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace SourceDocumentService
 {
@@ -7,11 +9,20 @@ namespace SourceDocumentService
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
