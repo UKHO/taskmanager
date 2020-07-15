79719c6ea5d635e9d8a5402701c5f964a9e7cdc1
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-15 11:59:33 +0100

    Db integrity tests over DbAssessmentVerify -> AdUsers

---
+++ src/Databases/WorkflowDatabase.Tests/AdUserTests/DbAssessmentVerifyTests.cs
@@ -0,0 +1,76 @@
+using System;
+using Common.Helpers;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using WorkflowDatabase.EF;
+using WorkflowDatabase.EF.Models;
+using WorkflowDatabase.Tests.Helpers;
+
+namespace WorkflowDatabase.Tests.AdUserTests
+{
+    public class DbAssessmentVerifyTests
+    {
+        private WorkflowDbContext _dbContext;
+        private AdUser TestUser1 { get; set; }
+        private AdUser TestUser2 { get; set; }
+        private WorkflowInstance SkeletonWorkflowInstance { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbContext = TestSetupHelper.CreateWorkflowDbContext();
+            DatabasesHelpers.ClearWorkflowDbTables(_dbContext);
+
+            TestUser1 = AdUserHelper.CreateTestUser(_dbContext);
+            TestUser2 = AdUserHelper.CreateTestUser(_dbContext, 2);
+
+            SkeletonWorkflowInstance = AdUserHelper.CreateSkeletonWorkflowInstance(_dbContext);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _dbContext.Dispose();
+        }
+
+        [Test]
+        public void Dbassessmentverifydata_table_prevents_unknown_revieweraduser_due_to_FK()
+        {
+            _dbContext.DbAssessmentVerifyData.Add(new DbAssessmentVerifyData
+            {
+                ReviewerAdUserId = int.MaxValue,
+                WorkflowInstanceId = SkeletonWorkflowInstance.WorkflowInstanceId
+            });
+
+            var ex = Assert.Throws<DbUpdateException>(() => _dbContext.SaveChanges());
+            Assert.That(ex.InnerException.Message.Contains("The INSERT statement conflicted with the FOREIGN KEY constraint", StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void Dbassessmentverifydata_table_prevents_unknown_assessoraduser_due_to_FK()
+        {
+            _dbContext.DbAssessmentVerifyData.Add(new DbAssessmentVerifyData
+            {
+                AssessorAdUserId = int.MaxValue,
+                WorkflowInstanceId = SkeletonWorkflowInstance.WorkflowInstanceId
+            });
+
+            var ex = Assert.Throws<DbUpdateException>(() => _dbContext.SaveChanges());
+            Assert.That(ex.InnerException.Message.Contains("The INSERT statement conflicted with the FOREIGN KEY constraint", StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void Dbassessmentverifydata_table_prevents_unknown_verifieraduser_due_to_FK()
+        {
+            _dbContext.DbAssessmentVerifyData.Add(new DbAssessmentVerifyData
+            {
+                VerifierAdUserId = int.MaxValue,
+                WorkflowInstanceId = SkeletonWorkflowInstance.WorkflowInstanceId
+            });
+
+            var ex = Assert.Throws<DbUpdateException>(() => _dbContext.SaveChanges());
+            Assert.That(ex.InnerException.Message.Contains("The INSERT statement conflicted with the FOREIGN KEY constraint", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
+
