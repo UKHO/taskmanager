323970ab670d5aa584da37c505013cea12f1d7c9
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-15 11:29:07 +0100

    Db tests for AD user FK for Assess + Review

---
+++ src/Databases/WorkflowDatabase.Tests/Helpers/TestSetupHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkflowDatabase.EF;
+
+namespace WorkflowDatabase.Tests.Helpers
+{
+    internal static class TestSetupHelper
+    {
+        internal static WorkflowDbContext CreateWorkflowDbContext()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<WorkflowDbContext>()
+               .UseSqlServer(
+                   @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=taskmanager-dev-workflowdatabase;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False")
+               .Options;
+
+            return new WorkflowDbContext(dbContextOptions);
+        }
+    }
+}
