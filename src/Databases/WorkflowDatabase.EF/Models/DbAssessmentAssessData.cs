fe506d164a0865cc0d18d0b6f9e82aca75be7a94
Ben Hall <3179852+benbhall@users.noreply.github.com>
2020-07-17 10:40:48 +0100

    Clean up virtuals in EF models.

--- src/Databases/WorkflowDatabase.EF/Models/DbAssessmentAssessData.cs
+++ src/Databases/WorkflowDatabase.EF/Models/DbAssessmentAssessData.cs
@@ -10,17 +10,18 @@ namespace WorkflowDatabase.EF.Models
         public string Ion { get; set; }
         public string ActivityCode { get; set; }
         public string SourceCategory { get; set; }
-        public virtual AdUser Reviewer { get; set; }
-        public int ReviewerAdUserId { get; set; }
-        public virtual AdUser Assessor { get; set; }
-        public int? AssessorAdUserId { get; set; }
-        public virtual AdUser Verifier { get; set; }
-        public int VerifierAdUserId { get; set; }
         public string TaskType { get; set; }
         public int WorkflowInstanceId { get; set; }
         public bool ProductActioned { get; set; }
         public string ProductActionChangeDetails { get; set; }
         [DisplayName("Caris Workspace:")]
         public string WorkspaceAffected { get; set; }
+
+        public virtual AdUser Reviewer { get; set; }
+        public int ReviewerAdUserId { get; set; }
+        public virtual AdUser Assessor { get; set; }
+        public int? AssessorAdUserId { get; set; }
+        public virtual AdUser Verifier { get; set; }
+        public int VerifierAdUserId { get; set; }
     }
 }
--- src/Databases/WorkflowDatabase.EF/Models/DbAssessmentReviewData.cs
+++ src/Databases/WorkflowDatabase.EF/Models/DbAssessmentReviewData.cs
@@ -10,21 +10,21 @@ namespace WorkflowDatabase.EF.Models
         public string Ion { get; set; }
         public string ActivityCode { get; set; }
         public string SourceCategory { get; set; }
+        [DisplayName("Task Type:")]
+        public string TaskType { get; set; }
+        [DisplayName("Workspace Affected:")]
+        public string WorkspaceAffected { get; set; }
+        [DisplayName("Notes:")]
+        public string Notes { get; set; }
+        public int WorkflowInstanceId { get; set; }
+
         [DisplayName("Assessor:")]
         public virtual AdUser Assessor { get; set; }
         public int? AssessorAdUserId { get; set; }
         [DisplayName("Verifier:")]
         public virtual AdUser Verifier { get; set; }
         public int? VerifierAdUserId { get; set; }
-        [DisplayName("Task Type:")]
-        public string TaskType { get; set; }
-        [DisplayName("Workspace Affected:")]
-        public string WorkspaceAffected { get; set; }
         public virtual AdUser Reviewer { get; set; }
         public int? ReviewerAdUserId { get; set; }
-
-        [DisplayName("Notes:")]
-        public string Notes { get; set; }
-        public int WorkflowInstanceId { get; set; }
     }
 }
