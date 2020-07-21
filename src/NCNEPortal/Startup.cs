bce1b1d5495a2087b54b57f640f0a337cb53d3b8
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-07-21 15:53:13 +0100

    Remove seemingly not needed ncneworkflowdbcontext arg

--- src/NCNEPortal/Startup.cs
+++ src/NCNEPortal/Startup.cs
@@ -147,8 +147,7 @@ namespace NCNEPortal
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app,
-            IWebHostEnvironment env,
-            NcneWorkflowDbContext ncneWorkflowDbContext)
+            IWebHostEnvironment env)
         {
             app.UseSerilogRequestLogging(
                 options =>
