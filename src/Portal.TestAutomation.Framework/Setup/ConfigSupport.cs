ca66cd69fb173e7dce6329955b808fcf75bf7730
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-07-27 18:36:21 +0100

    Removed SaveChanges

--- src/Portal.TestAutomation.Framework/Setup/ConfigSupport.cs
+++ src/Portal.TestAutomation.Framework/Setup/ConfigSupport.cs
@@ -68,8 +68,7 @@ namespace Portal.TestAutomation.Framework.Setup
 
             TestData.UsingDbConnectionString(workflowDbConnectionString)
                 .AddUser(_secrets.LoginAccount)
-                .ReassignReviewsToUser(_secrets.LoginAccount)
-                .SaveChanges();
+                .ReassignReviewsToUser(_secrets.LoginAccount);
         }
 
         private static void InitialiseWorkflowDbContext(string workflowDbConnectionString)
