693c240eeb523e924d72e8cd90bd195bbbe78103
Rossall Sandford <12933990+Rossall82@users.noreply.github.com>
2020-08-04 13:44:39 +0100

    Add UI test for New Task page

---
+++ src/DbUpdatePortal.TestAutomation.Steps/NewTaskPageSteps.cs
@@ -0,0 +1,30 @@
+﻿using DbUpdatePortal.TestAutomation.Framework.Pages;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DbUpdatePortal.TestAutomation.Steps
+{
+    [Binding]
+    public class NewTaskPageSteps
+    {
+        private readonly NewTaskPage _newtaskPage;
+
+        public NewTaskPageSteps(NewTaskPage newTaskPage)
+        {
+            _newtaskPage = newTaskPage;
+        }
+
+        [Given(@"I navigate to the Db Update New Task page")]
+        public void GivenINavigateToTheDbUpdateNewTaskPage()
+        {
+            _newtaskPage.NavigateTo();
+        }
+
+        [Then(@"The Db Update New Task page has loaded")]
+        public void ThenTheDbUpdateNewTaskPageHasLoaded()
+        {
+            Assert.IsTrue(_newtaskPage.HasLoaded);
+        }
+
+    }
+}
