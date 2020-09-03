e1d132d416965a54a8ab2c54c98eee16106ede4a
Bonnie Poole <bonnie.poole@ukho.gov.uk>
2020-09-03 11:14:08 +0100

    Automated test to checck db update historical tasks page will open

---
+++ src/DbUpdatePortal.TestAutomation.Framework/Pages/HistoricalTaskPage.cs
@@ -0,0 +1,36 @@
+﻿using Common.TestAutomation.Framework.Pages;
+using DbUpdatePortal.TestAutomation.Framework.Configs;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+
+namespace DbUpdatePortal.TestAutomation.Framework.Pages
+{
+    public class HistoricalTaskPage : PageBase, IHistoricalTaskPage
+    {
+        public HistoricalTaskPage(IWebDriver driver, WebDriverWait wait, UrlsConfig urlsConfig)
+            : base(driver, wait, urlsConfig.DbUpdateHistoricalTaskPageUrl)
+        {
+        }
+
+        private IWebElement UkhoLogo => Driver.FindElement(By.Id("ukhoLogo"));
+
+        public override bool HasLoaded
+        {
+            get
+            {
+                try
+                {
+                    Wait.Until(driver => UkhoLogo.Displayed);
+                    return true;
+                }
+                catch (NoSuchElementException e)
+                {
+                    Console.WriteLine(e);
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ src/DbUpdatePortal.TestAutomation.Steps/HistoricalTaskPageSteps.cs
@@ -0,0 +1,30 @@
+﻿using DbUpdatePortal.TestAutomation.Framework.Pages;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DbUpdatePortal.TestAutomation.Steps
+{
+    [Binding]
+    public class HistoricalTaskPageSteps
+    {
+        private readonly HistoricalTaskPage _historicalTaskPage;
+
+        public HistoricalTaskPageSteps(HistoricalTaskPage historicalTaskPage)
+        {
+            _historicalTaskPage = historicalTaskPage;
+        }
+
+        [Given(@"I navigate to the Db Update Historical Tasks page")]
+        public void GivenINavigateToTheDbUpdateHistoricalTasksPage()
+        {
+            _historicalTaskPage.NavigateTo(); 
+        }
+
+        [Then(@"The Db Update Historical Tasks page has loaded")]
+        public void ThenTheDbUpdateHistoricalTasksPageHasLoaded()
+        {
+            Assert.IsTrue(_historicalTaskPage.HasLoaded);
+        }
+
+    }
+}
