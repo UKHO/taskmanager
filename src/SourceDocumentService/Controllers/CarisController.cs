c555613aa959ca7b9cc98cabc715e44c56ecb1f2
Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
2020-10-07 12:34:26 +0100

    Added new controller used for Caris

---
+++ src/SourceDocumentService/Controllers/CarisController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SourceDocumentService.Controllers
+{
+    [ApiController]
+    public class CarisController : ControllerBase
+    {
+        [Route("/CarisService/v1/GetNewWreckageId")]
+        [HttpGet]
+        public IActionResult GetNewWreckageId()
+        {
+            return Ok(int.MaxValue);
+        }
+    }
+}
