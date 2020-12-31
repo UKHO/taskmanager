39ef6583586a74f602f4751461e30609379a59a6
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2020-12-31 09:43:25 +0000

    Use new Microsoft.Identity.Client for authorisation

--- src/Portal/Portal/Program.cs
+++ src/Portal/Portal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Common.Helpers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -39,6 +40,43 @@ namespace Portal
                     var azureAppConfConnectionString = Environment.GetEnvironmentVariable("AZURE_APP_CONFIGURATION_CONNECTION_STRING");
 
                     var (keyVaultAddress, keyVaultClient) = SecretsHelpers.SetUpKeyVaultClient();
+
+                    builder.AddInMemoryCollection(
+                        new Dictionary<string, string>
+                        {
+                            {"AzureAd:Instance", "https://login.microsoftonline.com/"},
+                            {"AzureAd:TenantId", "TENANT_ID_HERE"},
+                            {"AzureAd:ClientId", "CLIENT_ID_HERE"}
+                            //              3   Skipped
+                            //{"AzureAd:CallbackPath", "a"},
+                            //("AzureAd:SignedOutCallbackPath", "a"}
+                        }
+                    );
+
+                    /*
+                     * "AzureAd": {
+    // Azure cloud instance among:
+    // - "https://login.microsoftonline.com/" for Azure public cloud
+    // - "https://login.microsoftonline.us/" for Azure US government
+    // - "https://login.microsoftonline.de/" for Azure AD Germany
+    // - "https://login.chinacloudapi.cn/" for Azure AD China operated by 21Vianet
+    "Instance": "https://login.microsoftonline.com/",
+
+    // Azure AD audience among:
+    // - "TenantId" as a GUID obtained from the Azure portal to sign in users in your organization
+    // - "organizations" to sign in users in any work or school account
+    // - "common" to sign in users with any work or school account or Microsoft personal account
+    // - "consumers" to sign in users with a Microsoft personal account only
+    "TenantId": "[Enter the tenantId here]",
+
+    // Client ID (application ID) obtained from the Azure portal
+    "ClientId": "[Enter the Client Id]",
+    "CallbackPath": "/signin-oidc",
+    "SignedOutCallbackPath": "/signout-oidc"
+  }
+                     */
+
+
 
                     builder.AddAzureAppConfiguration(azureAppConfConnectionString)
                     .AddAzureKeyVault(keyVaultAddress, keyVaultClient, new DefaultKeyVaultSecretManager()).Build();
